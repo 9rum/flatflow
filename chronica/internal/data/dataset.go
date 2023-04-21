ca2b40d533d188f1049af0a24228551bff6ca546
9rum <sanha.maeng@kakao.com>
2023-04-21 17:44:04 +0900

    data: add Dataset interface scheduler: move BatchSize to Dataset

--- chronica/internal/data/dataset.go
+++ chronica/internal/data/dataset.go
@@ -17,3 +17,102 @@
 // it supports a partitioned dataset where the data is split into
 // multiple data partitions across nodes in the cluster.
 package data
+
+import "github.com/9rum/chronica/internal/btree"
+
+// Dataset represents the given dataset.
+type Dataset interface {
+	// BatchSize provides a primitive for the number of data samples to select
+	// at each step.
+	BatchSize() int
+
+	// Len provides a primitive for the number of times to schedule.
+	Len() int
+
+	// GetItem provides a mechanism to retrieve a data sample with the given
+	// arguments.  This must provide an index identifying the scheduled
+	// data sample.
+	GetItem(rank, size int) (index int)
+
+	// OnBatchEnd provides a mechanism to be called at the end of every step.
+	OnBatchEnd()
+
+	// OnEpochEnd provides a mechanism to be called at the end of every epoch.
+	OnEpochEnd()
+
+	// OnTrainEnd provides a mechanism to be called at the end of training.
+	OnTrainEnd()
+}
+
+// ShardedDataset represents a sharded dataset where every node in the cluster
+// has a replica of the given dataset; hence it ignores rank when looking for
+// the data sample.
+type ShardedDataset[T btree.Item] struct {
+	length     int
+	batchSize  int
+	shuffle    bool
+	dropLast   bool
+	items      *btree.BTree[T]
+	recycleBin *btree.BTree[T]
+}
+
+// NewShardedDataset creates a new sharded dataset with the given arguments.
+func NewShardedDataset[T btree.Item](sizes []int, batchSize int, shuffle, dropLast bool) *ShardedDataset[T] {
+	dataset := &ShardedDataset[T]{
+		batchSize: batchSize,
+		shuffle:   shuffle,
+		dropLast:  dropLast,
+	}
+
+	if len(sizes)%batchSize == 0 || dropLast {
+		dataset.length = len(sizes) / batchSize
+	} else {
+		dataset.length = len(sizes)/batchSize + 1
+	}
+
+	// We use the default degree for the nodes to fit on a single page.
+	dataset.items = btree.New[T](0)
+	dataset.recycleBin = btree.New[T](0)
+
+	for index, size := range sizes {
+		if _, found := dataset.items.ReplaceOrInsert(btree.NewItem[T](index, size)); found {
+			panic("insert found item")
+		}
+	}
+
+	return dataset
+}
+
+// BatchSize returns the batch size.
+func (d ShardedDataset[T]) BatchSize() int {
+	return d.batchSize
+}
+
+// Len returns the number of remaining steps currently in the training epoch.
+func (d ShardedDataset[T]) Len() int {
+	return d.length
+}
+
+// GetItem looks for the data sample with the size nearest to the given size.
+func (d *ShardedDataset[T]) GetItem(rank, size int) (index int) {
+	if item, ok := d.items.DeleteNearest(btree.NewItem[T](size, size)); !ok {
+		panic("didn't find item")
+	} else {
+		index = item.Index()
+		if _, found := d.recycleBin.ReplaceOrInsert(item); found {
+			panic("insert found item")
+		}
+	}
+	return
+}
+
+// PartitionedDataset represents a partitioned dataset where each of the nodes
+// in the cluster holds only a portion of the given dataset.
+type PartitionedDataset[T btree.Item] struct {
+	length      int
+	batchSize   int
+	shuffle     bool
+	dropLast    bool
+	partitions  []*btree.BTree[T]
+	recycleBins []*btree.BTree[T]
+}
