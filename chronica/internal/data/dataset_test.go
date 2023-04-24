42f9810ec3df2d7114d6989bbc3a7f77275cbbdf
9rum <sanha.maeng@kakao.com>
2023-04-25 00:07:04 +0900

    data: add unit test and benchmark for ShardedDataset

---
+++ chronica/internal/data/dataset_test.go
@@ -0,0 +1,97 @@
+// Copyright 2022 Sogang University
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package data
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/9rum/chronica/internal/btree"
+)
+
+func init() {
+	seed := time.Now().Unix()
+	fmt.Println(seed)
+	rand.Seed(seed)
+}
+
+func TestShardedDataset(t *testing.T) {
+	const (
+		datasetSize = 10000
+		batchSize   = 10
+		n           = 10
+	)
+
+	sizes := rand.Perm(datasetSize)
+
+	var dataset Dataset = NewShardedDataset[*btree.ItemBase](sizes)
+
+	for epoch := 0; epoch < n; epoch++ {
+		for step := 0; step < datasetSize/batchSize; step++ {
+			for index := step * batchSize; index < (step+1)*batchSize; index++ {
+				if dataset.Getitem(sizes[index], sizes[index]) != index {
+					t.Fatalf("did not find %d", sizes[index])
+				}
+			}
+		}
+		dataset.OnEpochEnd()
+	}
+	dataset.OnTrainEnd()
+
+	for size := range sizes {
+		sizes[size] = size
+	}
+
+	dataset = NewShardedDataset[*btree.ItemBase](sizes)
+
+	for epoch := 0; epoch < n; epoch++ {
+		for step := 0; step < datasetSize/batchSize; step++ {
+			for index := step * batchSize; index < (step+1)*batchSize; index++ {
+				if dataset.Getitem(sizes[index], sizes[index]) != index {
+					t.Fatalf("did not find %d", sizes[index])
+				}
+			}
+		}
+		dataset.OnEpochEnd()
+	}
+	dataset.OnTrainEnd()
+}
+
+func BenchmarkShardedDataset(b *testing.B) {
+	b.StopTimer()
+
+	const (
+		benchmarkDatasetSize = 10000
+		batchSize            = 10
+	)
+
+	sizes := rand.Perm(benchmarkDatasetSize)
+
+	b.StartTimer()
+
+	var dataset Dataset = NewShardedDataset[*btree.ItemBase](sizes)
+
+	for epoch := 0; epoch < b.N; epoch++ {
+		for step := 0; step < benchmarkDatasetSize/batchSize; step++ {
+			for index := step * batchSize; index < (step+1)*batchSize; index++ {
+				dataset.Getitem(sizes[index], sizes[index])
+			}
+		}
+		dataset.OnEpochEnd()
+	}
+	dataset.OnTrainEnd()
+}
