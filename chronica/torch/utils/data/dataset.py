4d6b45d25d50f455a7213f55d028a8e61a8a0198
9rum <sanha.maeng@kakao.com>
2023-05-07 00:54:41 +0900

    data: remove unnecessary imports and classes

--- chronica/torch/utils/data/dataset.py
+++ chronica/torch/utils/data/dataset.py
@@ -1,36 +1,21 @@
 import bisect
 import warnings
-import math
 from typing import (
     Generic,
     Iterable,
     Iterator,
     List,
-    Optional,
-    Sequence,
-    Tuple,
-    TypeVar,
-    Union
+    TypeVar
 )
-
-# No 'default_generator' in torch/__init__.pyi
-from torch import default_generator, randperm
-from torch._utils import _accumulate
-
-from ... import Generator, Tensor
 
 __all__ = [
     "Dataset",
     "IterableDataset",
-    "TensorDataset",
     "ConcatDataset",
     "ChainDataset",
-    "Subset",
-    "random_split",
 ]
 
 T_co = TypeVar('T_co', covariant=True)
-T = TypeVar('T')
 
 
 class Dataset(Generic[T_co]):
@@ -188,27 +173,6 @@ class IterableDataset(Dataset[T_co]):
     # See NOTE [ Lack of Default `__len__` in Python Abstract Base Classes ]
 
 
-class TensorDataset(Dataset[Tuple[Tensor, ...]]):
-    r"""Dataset wrapping tensors.
-
-    Each sample will be retrieved by indexing tensors along the first dimension.
-
-    Args:
-        *tensors (Tensor): tensors that have the same size of the first dimension.
-    """
-    tensors: Tuple[Tensor, ...]
-
-    def __init__(self, *tensors: Tensor) -> None:
-        assert all(tensors[0].size(0) == tensor.size(0) for tensor in tensors), "Size mismatch between tensors"
-        self.tensors = tensors
-
-    def __getitem__(self, index):
-        return tuple(tensor[index] for tensor in self.tensors)
-
-    def __len__(self):
-        return self.tensors[0].size(0)
-
-
 class ConcatDataset(Dataset[T_co]):
     r"""Dataset as a concatenation of multiple datasets.
 
@@ -232,7 +196,7 @@ class ConcatDataset(Dataset[T_co]):
     def __init__(self, datasets: Iterable[Dataset]) -> None:
         super().__init__()
         self.datasets = list(datasets)
-        assert len(self.datasets) > 0, 'datasets should not be an empty iterable'  # type: ignore[arg-type]
+        assert 0 < len(self.datasets), 'datasets should not be an empty iterable'  # type: ignore[arg-type]
         for d in self.datasets:
             assert not isinstance(d, IterableDataset), "ConcatDataset does not support IterableDataset"
         self.cumulative_sizes = self.cumsum(self.datasets)
@@ -242,7 +206,7 @@ class ConcatDataset(Dataset[T_co]):
 
     def __getitem__(self, idx):
         if idx < 0:
-            if -idx > len(self):
+            if len(self) < -idx:
                 raise ValueError("absolute value of index should not exceed dataset length")
             idx = len(self) + idx
         dataset_idx = bisect.bisect_right(self.cumulative_sizes, idx)
@@ -284,90 +248,3 @@ class ChainDataset(IterableDataset):
             assert isinstance(d, IterableDataset), "ChainDataset only supports IterableDataset"
             total += len(d)  # type: ignore[arg-type]
         return total
-
-
-class Subset(Dataset[T_co]):
-    r"""
-    Subset of a dataset at specified indices.
-
-    Args:
-        dataset (Dataset): The whole Dataset
-        indices (sequence): Indices in the whole set selected for subset
-    """
-    dataset: Dataset[T_co]
-    indices: Sequence[int]
-
-    def __init__(self, dataset: Dataset[T_co], indices: Sequence[int]) -> None:
-        self.dataset = dataset
-        self.indices = indices
-
-    def __getitem__(self, idx):
-        if isinstance(idx, list):
-            return self.dataset[[self.indices[i] for i in idx]]
-        return self.dataset[self.indices[idx]]
-
-    def __getitems__(self, indices: List[int]) -> List[T_co]:
-        # add batched sampling support when parent dataset supports it.
-        # see torch.utils.data._utils.fetch._MapDatasetFetcher
-        if callable(getattr(self.dataset, "__getitems__", None)):
-            return self.dataset.__getitems__([self.indices[idx] for idx in indices])  # type: ignore[attr-defined]
-        else:
-            return [self.dataset[self.indices[idx]] for idx in indices]
-
-    def __len__(self):
-        return len(self.indices)
-
-
-def random_split(dataset: Dataset[T], lengths: Sequence[Union[int, float]],
-                 generator: Optional[Generator] = default_generator) -> List[Subset[T]]:
-    r"""
-    Randomly split a dataset into non-overlapping new datasets of given lengths.
-
-    If a list of fractions that sum up to 1 is given,
-    the lengths will be computed automatically as
-    floor(frac * len(dataset)) for each fraction provided.
-
-    After computing the lengths, if there are any remainders, 1 count will be
-    distributed in round-robin fashion to the lengths
-    until there are no remainders left.
-
-    Optionally fix the generator for reproducible results, e.g.:
-
-    Example:
-        >>> # xdoctest: +SKIP
-        >>> generator1 = torch.Generator().manual_seed(42)
-        >>> generator2 = torch.Generator().manual_seed(42)
-        >>> random_split(range(10), [3, 7], generator=generator1)
-        >>> random_split(range(30), [0.3, 0.3, 0.4], generator=generator2)
-
-    Args:
-        dataset (Dataset): Dataset to be split
-        lengths (sequence): lengths or fractions of splits to be produced
-        generator (Generator): Generator used for the random permutation.
-    """
-    if math.isclose(sum(lengths), 1) and sum(lengths) <= 1:
-        subset_lengths: List[int] = []
-        for i, frac in enumerate(lengths):
-            if frac < 0 or frac > 1:
-                raise ValueError(f"Fraction at index {i} is not between 0 and 1")
-            n_items_in_split = int(
-                math.floor(len(dataset) * frac)  # type: ignore[arg-type]
-            )
-            subset_lengths.append(n_items_in_split)
-        remainder = len(dataset) - sum(subset_lengths)  # type: ignore[arg-type]
-        # add 1 to all the lengths in round-robin fashion until the remainder is 0
-        for i in range(remainder):
-            idx_to_add_at = i % len(subset_lengths)
-            subset_lengths[idx_to_add_at] += 1
-        lengths = subset_lengths
-        for i, length in enumerate(lengths):
-            if length == 0:
-                warnings.warn(f"Length of split at index {i} is 0. "
-                              f"This might result in an empty dataset.")
-
-    # Cannot verify that dataset is Sized
-    if sum(lengths) != len(dataset):    # type: ignore[arg-type]
-        raise ValueError("Sum of input lengths does not equal the length of the input dataset!")
-
-    indices = randperm(sum(lengths), generator=generator).tolist()  # type: ignore[call-overload]
-    return [Subset(dataset, indices[offset - length : offset]) for offset, length in zip(_accumulate(lengths), lengths)]
