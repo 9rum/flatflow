a92fe9f6ee56d3daef090e6f2880dab49e23f717
9rum <sanha.maeng@kakao.com>
2023-05-13 19:56:46 +0900

    cmd: implement scheduler server

---
+++ chronica/cmd/main.go
@@ -0,0 +1,63 @@
+// Copyright 2022 Sogang University
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package main implements the scheduler server. The initialization and
+// termination of the server may be invoked by the sampler, and the type of
+// scheduler and dataset is provided by the sampler.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/9rum/chronica/scheduler"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	port := flag.Int("p", 50051, "The server port")
+	flag.Parse()
+
+	if err := serve(*port); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func serve(port int) error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return err
+	}
+
+	server := newServer()
+
+	return server.Serve(lis)
+}
+
+func newServer() *grpc.Server {
+	server := grpc.NewServer()
+	done := make(chan os.Signal)
+
+	go func(done <-chan os.Signal, server *grpc.Server) {
+		<-done
+		server.GracefulStop()
+	}(done, server)
+
+	scheduler.RegisterSchedulerServer(server, scheduler.NewSchedulerServer(done))
+
+	return server
+}
