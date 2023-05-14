4c098bebb2be95411b18805d5202b96c1914780d
9rum <sanha.maeng@kakao.com>
2023-05-15 03:17:46 +0900

    scheduler: make scheduler server handle dataset callbacks use descrete values for arguments of binSize

--- chronica/scheduler/scheduler_grpc.go
+++ chronica/scheduler/scheduler_grpc.go
@@ -19,11 +19,14 @@ import (
 	"math"
 	"os"
 	"os/signal"
+	"runtime"
+	"sync"
 	"syscall"
 
 	"github.com/9rum/chronica/internal/btree"
 	"github.com/9rum/chronica/internal/data"
 	"github.com/golang/protobuf/ptypes/empty"
+	"golang.org/x/exp/constraints"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -31,6 +34,7 @@ import (
 // schedulerServer implements the server API for Scheduler service.
 type schedulerServer struct {
 	UnimplementedSchedulerServer
+	dataset   data.Dataset
 	scheduler Scheduler
 	done      chan<- os.Signal
 }
@@ -43,21 +47,22 @@ func NewSchedulerServer(done chan<- os.Signal) SchedulerServer {
 // Init initializes the training environment.
 func (s *schedulerServer) Init(ctx context.Context, in *Arguments) (*empty.Empty, error) {
 	// initialize a dataset with the given sizes
-	var dataset data.Dataset
+	sizes := cast(in.GetSizes())
+
 	if in.GetPartition() {
-		partitions := make([][]int64, 0, in.GetWorldSize())
-		for base := 0; base < len(in.GetSizes()); base += int(in.GetPartitionSize()) {
-			partitions = append(partitions, in.GetSizes()[base:base+int(in.GetPartitionSize())])
+		partitions := make([][]int, 0, in.GetWorldSize())
+		for base := 0; base < len(sizes); base += int(in.GetPartitionSize()) {
+			partitions = append(partitions, sizes[base:base+int(in.GetPartitionSize())])
 		}
-		dataset = data.NewPartitionedDataset[*btree.ItemBase](partitions)
+		s.dataset = data.NewPartitionedDataset[*btree.ItemBase](partitions)
 	} else {
-		dataset = data.NewShardedDataset[*btree.ItemBase](in.GetSizes())
+		s.dataset = data.NewShardedDataset[*btree.ItemBase](sizes)
 	}
 
 	// initialize a scheduler based on the given schedule type
 	switch in.GetType() {
 	case Schedule_STATIC:
-		s.scheduler = NewStaticScheduler(dataset, int(in.GetWorldSize()), int(in.GetBatchSize()), binSize(in))
+		s.scheduler = NewStaticScheduler(s.dataset, int(in.GetWorldSize()), int(in.GetBatchSize()), binSize(in.GetSizes(), in.GetWorldSize(), in.GetBatchSize()))
 	case Schedule_DYNAMIC:
 		fallthrough
 	default:
@@ -70,17 +75,34 @@ func (s *schedulerServer) Init(ctx context.Context, in *Arguments) (*empty.Empty
 	return new(empty.Empty), nil
 }
 
-// binSize returns the bin size to be used for static scheduling.
-func binSize(in *Arguments) (_ int) {
-	if in.GetType() == Schedule_STATIC {
-		return int(math.Round(mean(in.GetSizes()) * float64(in.GetBatchSize()) / float64(in.GetWorldSize())))
+// cast casts the given sizes to integer.
+func cast[T constraints.Integer](sizes []T) []int {
+	out := make([]int, len(sizes))
+	stride := int(math.Ceil(float64(len(sizes)) / float64(runtime.NumCPU())))
+
+	var wg sync.WaitGroup
+	for base := 0; base < len(sizes); base += stride {
+		wg.Add(1)
+		go func(base int) {
+			defer wg.Done()
+			for index := base; index < base+stride; index++ {
+				out[index] = int(sizes[index])
+			}
+		}(base)
 	}
-	return
+	wg.Wait()
+
+	return out
+}
+
+// binSize returns the bin size to be used for static scheduling.
+func binSize[T constraints.Integer](sizes []T, worldSize, batchSize T) int {
+	return int(math.Round(mean(sizes) * float64(batchSize) / float64(worldSize)))
 }
 
 // mean averages the given sizes.
-func mean(sizes []int64) float64 {
-	sum := int64(0)
+func mean[T constraints.Integer](sizes []T) float64 {
+	var sum T
 	for _, size := range sizes {
 		sum += size
 	}
@@ -96,6 +118,7 @@ func (s schedulerServer) Bcast(ctx context.Context, in *Feedback) (*Indices, err
 // the training environment for scheduling in the next training epoch.
 func (s schedulerServer) Reset(ctx context.Context, in *empty.Empty) (*empty.Empty, error) {
 	s.scheduler.OnEpochEnd()
+	s.dataset.OnEpochEnd()
 
 	if r := recover(); r != nil {
 		return nil, status.Errorf(codes.Internal, "%v", r)
@@ -112,6 +135,8 @@ func (s *schedulerServer) Finalize(ctx context.Context, in *empty.Empty) (*empty
 
 	s.scheduler.OnTrainEnd()
 	s.scheduler = nil
+	s.dataset.OnTrainEnd()
+	s.dataset = nil
 
 	if r := recover(); r != nil {
 		return nil, status.Errorf(codes.Internal, "%v", r)
