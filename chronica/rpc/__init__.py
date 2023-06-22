7fda8100e2032a2c0b5cbb5f418ad1917062c0dc
9rum <sanha.maeng@kakao.com>
2023-06-23 07:47:59 +0900

    rpc: move RPC utilities to separate directory

---
+++ chronica/rpc/__init__.py
@@ -0,0 +1,17 @@
+from chronica.rpc.scheduler_pb2 import (
+    Arguments,
+    DYNAMIC,
+    Feedback,
+    Schedule,
+    SCHEDULE,
+    STATIC,
+)
+from chronica.rpc.scheduler_pb2_grpc import SchedulerStub
+
+__all__ = ["Arguments",
+           "DYNAMIC",
+           "Feedback",
+           "Schedule",
+           "SCHEDULE",
+           "STATIC",
+           "SchedulerStub"]
