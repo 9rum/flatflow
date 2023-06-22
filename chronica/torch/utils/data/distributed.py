98f560c8d1efadac8ed191b8ad338ad7bc6ad250
9rum <sanha.maeng@kakao.com>
2023-06-23 07:48:50 +0900

    data: add draft of PyTorch plugin

--- chronica/torch/utils/data/distributed.py
+++ chronica/torch/utils/data/distributed.py
@@ -1,10 +1,14 @@
-import math
-from typing import TypeVar, Optional, Iterator
+import os
+import subprocess
+from typing import TypeVar, Optional, Iterable, Iterator
 
+import grpc
 import torch
 import torch.distributed as dist
 from torch.utils.data import Sampler
-from . import Dataset
+from chronica import sys
+from chronica.torch.utils.data import Dataset
+from chronica.rpc import Arguments, Feedback, Schedule, STATIC, DYNAMIC, SchedulerStub
 
 __all__ = ["DistributedSampler"]
 
@@ -16,8 +20,8 @@ class DistributedSampler(Sampler[T_co]):
 
     It is especially useful in conjunction with
     :class:`torch.nn.parallel.DistributedDataParallel`. In such a case, each
-    process can pass a :class:`~torch.utils.data.DistributedSampler` instance as a
-    :class:`~torch.utils.data.DataLoader` sampler, and load a subset of the
+    process can pass a :class:`~chronica.torch.utils.data.DistributedSampler` instance
+    as a :class:`~torch.utils.data.DataLoader` sampler, and load a subset of the
     original dataset that is exclusive to it.
 
     .. note::
@@ -32,26 +36,44 @@ class DistributedSampler(Sampler[T_co]):
         rank (int, optional): Rank of the current process within :attr:`num_replicas`.
             By default, :attr:`rank` is retrieved from the current distributed
             group.
-        shuffle (bool, optional): If ``True`` (default), sampler will shuffle the
-            indices.
-        seed (int, optional): random seed used to shuffle the sampler if
-            :attr:`shuffle=True`. This number should be identical across all
-            processes in the distributed group. Default: ``0``.
-        drop_last (bool, optional): if ``True``, then the sampler will drop the
+        shuffle (bool, optional): Not used but for PyTorch compatibility.
+        seed (int, optional): Random seed used to shuffle the sampler.
+            This number should be identical across all processes in the distributed group. Default: ``0``.
+        drop_last (bool, optional): If ``True``, then the sampler will drop the
             tail of the data to make it evenly divisible across the number of
             replicas. If ``False``, the sampler will add extra indices to make
             the data evenly divisible across the replicas. Default: ``False``.
+        batch_size (int, optional): How many samples per batch to load. Default: ``1``.
+        master_addr (str, optional): Address of the master node (rank 0).
+            If rendezvous protocol is enabled using ``torchrun``, the sampler automatically gets the address
+            from the environment variable.
+        master_port (int, optional): Port on the master node (rank 0) to be used for initializing
+            the scheduler server. Default: ``50051``.
+        schedule (str, optional): Schedule type (must be either ``"static"`` or ``"dynamic"``).
+            By default, ``"static"`` is set for static scheduling that reduces the workload imbalance between workers.
+            If ``"dynamic"``, the scheduler provides a feedback-directed optimization that adaptively adjusts
+            the workload on each worker.
+        interval (int, optional): Interval, in # of steps, to report the performance indicators for dynamic scheduling.
+        partition (bool, optional): If ``True``, then the sampler will restrict remote data fetching.
+            It is especially useful when the data is distributed among devices and machines. In such a case,
+            ``groups`` should tell the mapping about which workers are on which nodes. Default: ``False``.
+        groups (Iterable, optional): Mapping from worker rank to node rank. For instance, if the cluster is homogeneous
+            of two nodes with four GPUs each, ``groups`` would be ``[0, 0, 0, 0, 1, 1, 1, 1]``. On the other hand,
+            if the cluster is heterogeneous with four GPUs on node #0 and two GPUs on node #1,
+            then ``groups`` would be ``[0, 0, 0, 0, 1, 1]``.
 
     .. warning::
         In distributed mode, calling the :meth:`set_epoch` method at
         the beginning of each epoch **before** creating the :class:`DataLoader` iterator
-        is necessary to make shuffling work properly across multiple epochs. Otherwise,
-        the same ordering will be always used.
+        is necessary to make scheduling work properly across multiple epochs.
     """
 
     def __init__(self, dataset: Dataset, num_replicas: Optional[int] = None,
                  rank: Optional[int] = None, shuffle: bool = True,
-                 seed: int = 0, drop_last: bool = False) -> None:
+                 seed: int = 0, drop_last: bool = False, batch_size: int = 1,
+                 master_addr: str = None, master_port: int = 50051,
+                 schedule: str = None, interval: int = 1,
+                 partition: bool = False, groups: Iterable = None) -> None:
         if num_replicas is None:
             if not dist.is_available():
                 raise RuntimeError("Requires distributed package to be available")
@@ -61,66 +83,77 @@ class DistributedSampler(Sampler[T_co]):
                 raise RuntimeError("Requires distributed package to be available")
             rank = dist.get_rank()
         if num_replicas <= rank or rank < 0:
-            raise ValueError(
-                "Invalid rank {}, rank should be in the interval"
-                " [0, {}]".format(rank, num_replicas - 1))
-        self.dataset = dataset
-        self.num_replicas = num_replicas
+            raise ValueError("Invalid rank {}, rank should be in the interval [0, {}]".format(rank, num_replicas - 1))
+        if master_addr is None:
+            master_addr = os.getenv("MASTER_ADDR", "127.0.0.1")
+        if schedule is None or schedule == "static":
+            self.schedule = STATIC
+        elif schedule == "dynamic":
+            self.schedule = DYNAMIC
+        else:
+            raise ValueError("Invalid schedule type {}, schedule should be either static or dynamic".format(schedule))
         self.rank = rank
-        self.epoch = 0
-        self.drop_last = drop_last
+        self.interval = interval
+
         # If the dataset length is evenly divisible by # of replicas, then there
         # is no need to drop any data, since the dataset will be split equally.
-        if self.drop_last and len(self.dataset) % self.num_replicas != 0:  # type: ignore[arg-type]
-            # Split to nearest available length that is evenly divisible.
-            # This is to ensure each rank receives the same amount of data when
-            # using this Sampler.
-            self.num_samples = math.ceil(
-                (len(self.dataset) - self.num_replicas) / self.num_replicas  # type: ignore[arg-type]
-            )
+        self.num_samples = len(dataset) // num_replicas  # type: ignore[arg-type]
+        if not drop_last and len(dataset) % num_replicas != 0:  # type: ignore[arg-type]
+            self.num_samples += 1
+        total_size = self.num_samples * num_replicas
+
+        self.indices = list(range(len(dataset)))  # type: ignore[arg-type]
+        if drop_last:
+            # remove tail of data to make it evenly divisible.
+            self.indices = self.indices[:total_size]
         else:
-            self.num_samples = math.ceil(len(self.dataset) / self.num_replicas)  # type: ignore[arg-type]
-        self.total_size = self.num_samples * self.num_replicas
-        self.shuffle = shuffle
-        self.seed = seed
+            # add extra samples to make it evenly divisible.
+            padding_size = total_size - len(self.indices)
+            # deterministically shuffle based on seed.
+            g = torch.Generator()
+            g.manual_seed(seed)
+            if partition:
+                assert groups is not None
+                last = max(groups)
+                base = len(list(filter(lambda rank: rank < last, groups))) * self.num_samples
+                perm = torch.randperm(len(self.indices[base:]), generator=g).add(base).tolist()
+                if len(self.indices[base:]) < padding_size:
+                    self.indices += self.indices[base:] * (padding_size // len(self.indices[base:])) + perm[:padding_size % len(self.indices[base:])]
+                else:
+                    self.indices += perm[:padding_size]
+            else:
+                perm = torch.randperm(len(self.indices), generator=g).tolist()
+                if len(self.indices) < padding_size:
+                    self.indices += self.indices * (padding_size // len(self.indices)) + perm[:padding_size % len(self.indices)]
+                else:
+                    self.indices += perm[:padding_size]
+        assert len(self.indices) == total_size
+
+        self.stub = SchedulerStub(grpc.insecure_channel("{}:{}".format(master_addr, master_port)))
+
+        if self.rank == 0:
+            sizes = list(map(lambda index: sys.getsizeof(dataset, index), self.indices))
+            self.stub.Init(Arguments(num_replicas, batch_size, sizes, groups, partition, self.schedule))
 
     def __iter__(self) -> Iterator[T_co]:
-        if self.shuffle:
-            # deterministically shuffle based on epoch and seed
-            g = torch.Generator()
-            g.manual_seed(self.seed + self.epoch)
-            indices = torch.randperm(len(self.dataset), generator=g).tolist()  # type: ignore[arg-type]
-        else:
-            indices = list(range(len(self.dataset)))  # type: ignore[arg-type]
+        return self
 
-        if not self.drop_last:
-            # add extra samples to make it evenly divisible
-            padding_size = self.total_size - len(indices)
-            if padding_size <= len(indices):
-                indices += indices[:padding_size]
-            else:
-                indices += (indices * math.ceil(padding_size / len(indices)))[:padding_size]
-        else:
-            # remove tail of data to make it evenly divisible.
-            indices = indices[:self.total_size]
-        assert len(indices) == self.total_size
-
-        # subsample
-        indices = indices[self.rank:self.total_size:self.num_replicas]
-        assert len(indices) == self.num_samples
-
-        return iter(indices)
+    def __next__(self) -> T_co:
+        pass
 
     def __len__(self) -> int:
         return self.num_samples
 
+    def __del__(self) -> None:
+        if self.rank == 0:
+            self.stub.Finalize()
+
     def set_epoch(self, epoch: int) -> None:
         r"""
-        Sets the epoch for this sampler. When :attr:`shuffle=True`, this ensures all replicas
-        use a different random ordering for each epoch. Otherwise, the next iteration of this
-        sampler will yield the same ordering.
+        Sets the epoch for this sampler. This ensures scheduling work properly across multiple epochs.
 
         Args:
             epoch (int): Epoch number.
         """
-        self.epoch = epoch
+        if self.rank == 0 and 0 < epoch:
+            self.stub.Reset()
