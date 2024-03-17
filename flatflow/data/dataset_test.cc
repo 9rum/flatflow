702bd90dc1be231bad6cbebd87d6cf63b4d85010
amo33 <andylee70044@gmail.com>
2024-03-17 22:51:59 +0900

    dataset_test : Add Intrashuffle test

--- flatflow/data/dataset_test.cc
+++ flatflow/data/dataset_test.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "flatflow/data/dataset.h"
+
 
 #include <cstdlib>
 #include <ctime>
@@ -21,7 +21,9 @@
 
 #include <flatbuffers/flatbuffers.h>
 #include <gtest/gtest.h>
+#include <absl/container/inlined_vector.h>
 
+#include "flatflow/data/dataset.h"
 #include "flatflow/data/dataset_test.h"
 
 namespace {
@@ -49,6 +51,14 @@ class DatasetTest final : private flatflow::data::Dataset<uint64_t, uint16_t> {
   }
 
   inline bool empty() const noexcept { return recyclebin.empty(); }
+
+  inline void intra_shuffle(auto epoch){
+    on_epoch_begin(epoch);
+  }
+
+  inline std::vector<uint64_t> access_vector(uint16_t size) const noexcept{
+    return items.at(size);
+  }
 };
 
 TEST(DatasetTest, Constructor) {
@@ -90,6 +100,93 @@ TEST(DatasetTest, Constructor) {
     }
   }
   EXPECT_TRUE(dataset.empty());
+}
+
+TEST(DatasetTest, IntraShuffle) {
+  std::srand(static_cast<unsigned int>(std::time(nullptr)));
+  thread_local auto generator = std::ranlux48();
+  auto items = std::map<uint16_t, std::size_t>();
+  for (uint16_t size = 1; size <= 1 << 12; ++size) {
+    items.emplace(size, static_cast<std::size_t>(std::rand() % (1 << 15)));
+  }
+
+  auto sizes = std::vector<uint16_t>();
+  for (const auto item : items) {
+    const auto size = item.first;
+    auto count = item.second;
+    for (; 0 < count; --count) {
+      sizes.push_back(size);
+    }
+  }
+  sizes.shrink_to_fit();
+
+  auto builder = flatbuffers::FlatBufferBuilder64();
+  auto sizes__ = builder.CreateVector64(sizes);
+  auto offset = CreateSizes(builder, sizes__);
+  builder.Finish(offset);
+  auto sizes_ = GetSizes(builder.GetBufferPointer());
+  auto dataset = DatasetTest(sizes_->sizes(), 0UL);
+
+  for (const auto& item: items){
+    const auto& size = item.first;
+    EXPECT_TRUE(dataset.is_sorted(size));
+  }
+  // call on_epoch_begin for shuffle.
+  dataset.intra_shuffle(0); 
+  for (const auto& item: items){
+    const auto& size = item.first;
+    EXPECT_FALSE(dataset.is_sorted(size));
+  }
+  constexpr auto kIndexSlotSpace =
+    static_cast<std::size_t>(1 << std::numeric_limits<uint16_t>::digits);
+  auto counts =
+    absl::InlinedVector<uint64_t, kIndexSlotSpace>(kIndexSlotSpace, 0);
+
+  // clang-format off
+  #pragma omp unroll partial
+  // clang-format on
+  for (uint64_t index = 0; index < sizes.size(); ++index) {
+    const auto size = static_cast<std::size_t>(sizes[index]);
+    ++counts.at(size);
+  }
+
+  auto slots = absl::InlinedVector<std::vector<uint64_t>, kIndexSlotSpace>(
+      kIndexSlotSpace);
+
+  // clang-format off
+  #pragma omp parallel for
+  // clang-format on
+  for (std::size_t size = 0; size < counts.size(); ++size) {
+    const auto count = counts.at(size);
+    if (0 < count) {
+      slots.at(size).reserve(static_cast<std::size_t>(count));
+    }
+  }
+
+  // clang-format off
+  #pragma omp unroll partial
+  // clang-format on
+  for (uint64_t index = 0; index < sizes.size(); ++index) {
+    const auto size = static_cast<std::size_t>(sizes[index]);
+    slots.at(size).emplace_back(index);
+  }
+
+  //clang-format off
+  #pragma omp parallel for
+  // clang-format on
+  for (auto &item : slots) {
+    generator.seed(static_cast<uint_fast64_t>(0UL + 0));
+    std::shuffle(item.begin(), item.end(), generator);
+  }
+
+  for (std::size_t size = 0; size < counts.size(); ++size) {
+    const auto count = counts.at(size);
+    if (0 < count) {
+      const auto& dataset_vector = dataset.access_vector(size);
+      const auto& current_vector = slots.at(size);
+      EXPECT_TRUE(std::equal(dataset_vector.begin(),dataset_vector.end(),current_vector.begin()));
+    }
+  }
 }
 
 }  // namespace
