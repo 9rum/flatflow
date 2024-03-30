d1b7b3e3e65eb6fdb90877bae296927e92e6caa5
9rum <sanha.maeng@kakao.com>
2024-03-31 01:23:04 +0900

    scheduler: implement inter-batch shuffling

--- flatflow/scheduler/scheduler.h
+++ flatflow/scheduler/scheduler.h
@@ -15,6 +15,9 @@
 #ifndef FLATFLOW_SCHEDULER_SCHEDULER_H_
 #define FLATFLOW_SCHEDULER_SCHEDULER_H_
 
+#include <algorithm>
+#include <execution>
+#include <random>
 #include <utility>
 #include <vector>
 
@@ -86,52 +89,36 @@ class Scheduler {
     dataset_ = std::move(flatflow::data::Dataset(sizes, seed));
   }
 
-  /// \brief Assigns the next mini-batch to each of the workers. It adopts
-  /// first-fit-decreasing (FFD), a near-optimal heuristic for bin packing.
-  /// * FFD paper: https://dspace.mit.edu/bitstream/handle/1721.1/57819/17595570-MIT.pdf
-  /// * Python implementation: https://github.com/erelsgl/prtpy/blob/ebe54010513ea725f7a3221e4aa0258afa15d6fb/prtpy/packing/first_fit.py
-  /// \return The next mini-batch.
-  inline std::vector<std::vector<value_type>> schedule() {
-    const auto local_batch_size = step_++ == last_batch_
-                                      ? last_batch_size_ / world_size_
-                                      : batch_size_ / world_size_;
-    const auto bin_size =
-        static_cast<key_type>(std::round(mean_ * local_batch_size));
+  /// \brief Schedules and shuffles batches for the next training epoch to each
+  /// of the workers.
+  /// \param interval The scheduling interval, which is ignored in this schedule
+  /// kind as static scheduling occurs at the granularity of epoch.
+  /// \return Schedule for the next training epoch.
+  inline auto schedule([[maybe_unused]] value_type interval)
+      -> std::vector<std::vector<value_type>> {
+    const auto now = omp_get_wtime();
 
-    auto bins = std::vector<key_type>(static_cast<std::size_t>(world_size_), 0);
-    auto batches = std::vector<std::vector<value_type>>();
-    batches.reserve(static_cast<std::size_t>(world_size_));
+    auto batches = std::vector<std::vector<std::vector<value_type>>>();
+    batches.reserve(static_cast<std::size_t>(last_batch_ + 1));
 
-    // Pack the bins in a first-fit-decreasing fashion.
-    //
-    // NOTE:
-    //
-    // Assigning each worker an equal number of data samples with the same sum
-    // of sizes per batch is an online variation of multiway number partitioning
-    // with number constraints, so called balanced multiway number partitioning.
-    // As this problem is NP-hard, there are several approximation algorithms
-    // such as largest differencing method (LDM or Karmarkar–Karp algorithm).
-    // For now, we model this problem as bin packing and use FFD, an
-    // approximately-optimal heuristic for bin packing (its approximation ratio
-    // is 11/9). This can be changed in the future.
-    //
-    // * Partition problem: https://en.wikipedia.org/wiki/Partition_problem
-    // * Balanced number partitioning: https://en.wikipedia.org/wiki/Balanced_number_partitioning
-    // * Largest differencing method: https://en.wikipedia.org/wiki/Largest_differencing_method
-    // * Python implementation for Karmarkar–Karp algorithm: https://github.com/erelsgl/prtpy/blob/3ab0facffebc758c49bb3d06bd94a5f140a99863/prtpy/partitioning/karmarkar_karp.py
     for (; batches.size() < batches.capacity();) {
-      auto batch = std::vector<value_type>();
-      batch.reserve(static_cast<std::size_t>(local_batch_size));
-      for (; batch.size() < batch.capacity();) {
-        const auto [index, size] =
-            dataset_.find(bin_size - bins.at(batches.size()));
-        batch.emplace_back(index);
-        bins.at(batches.size()) += size;
-      }
-      batches.emplace_back(std::move(batch));
+      batches.emplace_back(schedule());
     }
 
-    return batches;
+    // After scheduling, a `flatflow::scheduler::Scheduler` shuffles between
+    // batches, which we call inter-batch shuffling. This enables shuffling not
+    // only between data samples with the same size but also between scheduled
+    // batches. It uses the same pseudorandom number generator and random seed
+    // as `flatflow::data::Dataset` for deterministic shuffling.
+    auto generator = std::ranlux48();
+    generator.seed(static_cast<uint_fast64_t>(seed_ + epoch_));
+    std::shuffle(batches.begin(), batches.end(), generator);
+
+    const auto indices = reshape(batches);
+
+    LOG(INFO) << absl::StrFormat("Scheduling %u steps took %f seconds", last_batch_ + 1, omp_get_wtime() - now);
+
+    return indices;
   }
 
   /// \brief A callback to be called at the beginning of a training batch.
@@ -152,6 +139,7 @@ class Scheduler {
   inline void on_epoch_begin(value_type epoch, value_type rank) {
     if (rank == 0) {
       step_ = 0;
+      epoch_ = epoch;
       dataset_.on_epoch_begin(epoch);
     }
   }
@@ -172,9 +160,85 @@ class Scheduler {
   inline void on_train_end() const noexcept { dataset_.on_train_end(); }
 
  protected:
+  /// \brief Assigns the next batch to each of the workers. It adopts
+  /// first-fit-decreasing (FFD), a near-optimal heuristic for bin packing.
+  /// * FFD paper: https://dspace.mit.edu/bitstream/handle/1721.1/57819/17595570-MIT.pdf
+  /// * Python implementation: https://github.com/erelsgl/prtpy/blob/ebe54010513ea725f7a3221e4aa0258afa15d6fb/prtpy/packing/first_fit.py
+  /// \return The next batch.
+  inline std::vector<std::vector<value_type>> schedule() {
+    const auto local_batch_size = step_++ == last_batch_
+                                      ? last_batch_size_ / world_size_
+                                      : batch_size_ / world_size_;
+    const auto bin_size =
+        static_cast<key_type>(std::round(mean_ * local_batch_size));
+
+    auto bins = std::vector<key_type>(static_cast<std::size_t>(world_size_), 0);
+    auto batches = std::vector<std::vector<value_type>>();
+    batches.reserve(static_cast<std::size_t>(world_size_));
+
+    // Pack the bins in a first-fit-decreasing fashion.
+    //
+    // NOTE:
+    //
+    // Assigning each worker an equal number of data samples with the same sum
+    // of sizes per batch is an online variation of multiway number partitioning
+    // with number constraints, so called balanced multiway number partitioning.
+    // As this problem is NP-hard, there are several approximation algorithms
+    // such as largest differencing method (LDM or Karmarkar–Karp algorithm).
+    // For now, we model this as a bin packing problem and use the FFD solver,
+    // an approximately-optimal heuristic for bin packing with approximation
+    // ratio of 11/9. The scheduling behavior may change in the future.
+    //
+    // * Partition problem: https://en.wikipedia.org/wiki/Partition_problem
+    // * Balanced number partitioning: https://en.wikipedia.org/wiki/Balanced_number_partitioning
+    // * Largest differencing method: https://en.wikipedia.org/wiki/Largest_differencing_method
+    // * Python implementation for Karmarkar–Karp algorithm: https://github.com/erelsgl/prtpy/blob/3ab0facffebc758c49bb3d06bd94a5f140a99863/prtpy/partitioning/karmarkar_karp.py
+    for (; batches.size() < batches.capacity();) {
+      auto batch = std::vector<value_type>();
+      batch.reserve(static_cast<std::size_t>(local_batch_size));
+      for (; batch.size() < batch.capacity();) {
+        const auto [index, size] =
+            dataset_.find(bin_size - bins.at(batches.size()));
+        batch.emplace_back(index);
+        bins.at(batches.size()) += size;
+      }
+      batches.emplace_back(std::move(batch));
+    }
+
+    return batches;
+  }
+
+  /// \brief Converts the given three-dimensional tensor to a corresponding
+  /// two-dimensional tensor, or a matrix.
+  /// \param tensor A three-dimensional tensor to convert.
+  /// \return The corresponding matrix.
+  inline std::vector<std::vector<value_type>> reshape(
+      const std::vector<std::vector<std::vector<value_type>>> &tensor) const {
+    auto matrix = std::vector<std::vector<value_type>>();
+    matrix.reserve(static_cast<std::size_t>(world_size_));
+
+    for (; matrix.size() < matrix.capacity();) {
+      const auto row = std::vector<value_type>(static_cast<std::size_t>(
+          (batch_size_ * last_batch_ + last_batch_size_) / world_size_));
+      matrix.emplace_back(std::move(row));
+    }
+
+    #pragma omp parallel for
+    for (std::size_t rank = 0; rank < matrix.size(); ++rank) {
+      auto dest = matrix.at(rank).begin();
+      std::for_each(std::execution::seq, tensor.cbegin(), tensor.cend(),
+                    [&](const auto &batch) {
+                      dest = std::copy(batch.at(rank).cbegin(),
+                                       batch.at(rank).cend(), dest);
+                    });
+    }
+
+    return matrix;
+  }
+
   double mean_;
   value_type world_size_, batch_size_, last_batch_, last_batch_size_, step_,
-      seed_;
+      epoch_, seed_;
   flatflow::data::Dataset<value_type, key_type> dataset_;
 };
 
