90516d6e3877b6cf7d536de2d06a6dcb8ad53389
amo33 <andylee70044@gmail.com>
2024-04-05 11:24:24 +0900

    scheduler : add static scheduler unit test

--- flatflow/scheduler/scheduler_test.cc
+++ flatflow/scheduler/scheduler_test.cc
@@ -12,24 +12,95 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "scheduler/scheduler.h"
+#include "flatflow/scheduler/scheduler.h"
 
 #include <cstdlib>
 #include <ctime>
 #include <map>
 #include <vector>
 
-#include "absl/container/inlined_vector.h"
+#include "absl/log/log.h"
+#include <absl/strings/str_join.h> // Add
+#include <absl/strings/str_format.h> // Add
 #include "flatbuffers/flatbuffers.h"
 #include "gtest/gtest.h"
 
+#include "flatflow/data/dataset_test.h"
+
 namespace {
-    class SchedulerTest final : private flatflow::Scheduler {
-    };
-    TEST(SchedulerTest, StaticSheduler){
+class SchedulerTest final
+    : private flatflow::scheduler::Scheduler<uint64_t, uint16_t> {
+ public:
+  inline SchedulerTest(const flatbuffers::Vector64<uint16_t> *sizes,
+                                uint64_t world_size, uint64_t batch_size, uint64_t seed)
+      : flatflow::scheduler::Scheduler<uint64_t, uint16_t>(
+            sizes, world_size, batch_size, seed) {}
+  inline auto on_schedule(uint64_t interval){
+    return schedule(interval);
+  }
+  inline void epoch_end(uint64_t epoch, uint64_t rank){
+    on_epoch_begin(epoch,rank);
+  }
+  inline void train_end(){
+    on_train_end();
+  }
+};
 
-    }
-    TEST(SchedulerTest, StaticSchedulerRemainder){
+TEST(SchedulerTest, StaticSchedule) {
+  std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
+  auto datasetsize = 1 << 10;
+  const auto world_size = 1 << 2;
+  auto batch_size = 1 << 5;
+  auto seed = 0UL;
+
+  auto items = std::map<uint16_t, std::size_t>();
+  for (uint16_t size = 1; size <= datasetsize; ++size) {
+    items.emplace(size, static_cast<std::size_t>(std::rand() % (datasetsize)));
+  }
+
+  auto sizes = std::vector<uint16_t>();
+  for (const auto item : items) {
+    const auto size = item.first;
+    auto count = item.second;
+    for (; 0 < count; --count) {
+      sizes.push_back(size);
     }
+  }
+  sizes.shrink_to_fit();
+
+  // As of FlatBuffers v24.3.7, it is not possible to initialize a 64-bit
+  // vector directly; use generated code from the FlatBuffers schema.
+  auto builder = flatbuffers::FlatBufferBuilder64();
+  auto sizes__ = builder.CreateVector64(sizes);
+  auto offset = CreateSizes(builder, sizes__);
+  builder.Finish(offset);
+  auto sizes_ = GetSizes(builder.GetBufferPointer());
+  // auto dataset = flatflow::data::Dataset(sizes_->sizes(), seed);
+
+  auto scheduler = SchedulerTest(sizes_->sizes(), world_size, batch_size, seed);
+  LOG(INFO) << "New version ";
+  for (auto epoch = 0; epoch < 10; ++epoch) {
+    for (auto rank = 0; rank < world_size; ++rank) {
+      scheduler.epoch_end(epoch, rank);
+    }
+    for (auto step = 0; step < datasetsize / batch_size; ++step) {
+      const auto& indices = scheduler.on_schedule(step);
+
+      std::vector<uint64_t> sums;
+      LOG(INFO) << "Calculating sums for step " << step << ": ";
+      for (auto& indice : indices) {
+          auto value = 0;
+          for (const auto index : indice) {
+              value += sizes_->sizes()->Get(index);
+          }
+          sums.push_back(value);
+      }
+
+      std::string vector_str = absl::StrJoin(sums, ", ");
+      LOG(INFO) << " step : " << step << "\t got : [" << vector_str << "]\n";
+    }
+  }
+  scheduler.train_end();
 }
+}  // namespace
