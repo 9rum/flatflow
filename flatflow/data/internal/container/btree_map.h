9f77233941911a8dfd065bc2ab47c8d2c36c6d8d
9rum <sanha.maeng@kakao.com>
2024-05-01 17:11:54 +0900

    container: make `swap` always be inlined

--- flatflow/data/internal/container/btree_map.h
+++ flatflow/data/internal/container/btree_map.h
@@ -488,7 +488,7 @@ class btree_map
 //
 // Swaps the contents of two `btree_map` containers.
 template <typename K, typename V, typename C, typename A, int S>
-inline void swap(btree_map<K, V, C, A, S> &map, btree_map<K, V, C, A, S> &other) {
+ABSL_ATTRIBUTE_ALWAYS_INLINE inline void swap(btree_map<K, V, C, A, S> &map, btree_map<K, V, C, A, S> &other) {
   map.swap(other);
 }
 
@@ -823,7 +823,7 @@ class btree_multimap
 //
 // Swaps the contents of two `btree_multimap` containers.
 template <typename K, typename V, typename C, typename A, int S>
-inline void swap(btree_multimap<K, V, C, A, S> &map, btree_multimap<K, V, C, A, S> &other) {
+ABSL_ATTRIBUTE_ALWAYS_INLINE inline void swap(btree_multimap<K, V, C, A, S> &map, btree_multimap<K, V, C, A, S> &other) {
   map.swap(other);
 }
 
