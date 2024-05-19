2d33d8cc1ffc8dac02b28182e57a9a092b66ad8c
9rum <sanha.maeng@kakao.com>
2024-05-19 23:35:52 +0900

    algorithm: implement Karmarkar-Karp algorithm

--- flatflow/scheduler/internal/algorithm/partition.h
+++ flatflow/scheduler/internal/algorithm/partition.h
@@ -15,10 +15,13 @@
 #ifndef FLATFLOW_SCHEDULER_INTERNAL_ALGORITHM_PARTITION_H_
 #define FLATFLOW_SCHEDULER_INTERNAL_ALGORITHM_PARTITION_H_
 
+#include <omp.h>
+
 #include <algorithm>
 #include <iterator>
 #include <queue>
 #include <span>
+#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -26,6 +29,9 @@
 #include "absl/log/check.h"
 #include "absl/log/log.h"
 #include "absl/random/internal/platform.h"
+#include "absl/strings/str_format.h"
+
+#include "flatflow/data/internal/types.h"
 
 namespace flatflow {
 namespace scheduler {
@@ -36,20 +42,32 @@ namespace {
 
 // Subset<>
 //
-//
+// Structure that stores a partition created by the Karmarkar-Karp algorithm.
+// Note that unlike in the original partition problem, this structure does not
+// store the items but only the subset sum.
+template <typename Index, typename Size, typename UnaryOp>
+  requires(flatflow::data::internal::Unsigned<Index> &&
+           flatflow::data::internal::Unsigned<Size> &&
+           std::is_invocable_v<UnaryOp, Size>)
 struct Subset {
+  using key_type = Size;
+  using mapped_type = Index;
+  using result_type = std::invoke_result_t<UnaryOp, Size>;
+
   inline ABSL_ATTRIBUTE_ALWAYS_INLINE explicit Subset(
-      const std::pair<const uint16_t, uint64_t> &item) {
-    sum = item.first;
-    indices = std::vector<uint64_t>(1, item.second);
+      const std::pair<const key_type, mapped_type> &ABSL_RANDOM_INTERNAL_RESTRICT item,
+      UnaryOp op) {
+    sum = op(item.first);
+    indices = std::vector<mapped_type>(1, item.second);
   }
 
   inline ABSL_ATTRIBUTE_ALWAYS_INLINE bool operator<(
-      const Subset &other) const noexcept {
+      const Subset &ABSL_RANDOM_INTERNAL_RESTRICT other) const noexcept {
     return sum < other.sum;
   }
 
-  inline ABSL_ATTRIBUTE_ALWAYS_INLINE void Join(const Subset &other) {
+  inline ABSL_ATTRIBUTE_ALWAYS_INLINE void Join(
+      const Subset &ABSL_RANDOM_INTERNAL_RESTRICT other) {
     sum += other.sum;
     indices.reserve(indices.size() + other.indices.size());
     indices.insert(indices.end(),
@@ -57,38 +75,50 @@ struct Subset {
                    std::make_move_iterator(other.indices.end()));
   }
 
-  uint_fast64_t sum;
-  std::vector<uint64_t> indices;
+  result_type sum;
+  std::vector<mapped_type> indices;
 };
 
 // Solution<>
 //
-//
+// Structure that holds a partial solution used in the Karmarkar-Karp algorithm.
+template <typename Index, typename Size, typename UnaryOp>
+  requires(flatflow::data::internal::Unsigned<Index> &&
+           flatflow::data::internal::Unsigned<Size> &&
+           std::is_invocable_v<UnaryOp, Size>)
 struct Solution {
+  using key_type = Size;
+  using mapped_type = Index;
+  using result_type = std::invoke_result_t<UnaryOp, Size>;
+
   ABSL_ATTRIBUTE_NOINLINE explicit Solution(
-      std::span<const std::pair<const uint16_t, uint64_t>> items) {
+      std::span<const std::pair<const key_type, mapped_type>> items,
+      UnaryOp op) {
     subsets.reserve(items.size());
     for (const auto &item : items) {
-      subsets.emplace_back(Subset(item));
+      subsets.emplace_back(item, op);
     }
     difference = subsets.at(subsets.size() - 1).sum - subsets.at(0).sum;
   }
 
   inline ABSL_ATTRIBUTE_ALWAYS_INLINE bool operator<(
-      const Solution &other) const noexcept {
+      const Solution &ABSL_RANDOM_INTERNAL_RESTRICT other) const noexcept {
     return difference < other.difference;
   }
 
-  ABSL_ATTRIBUTE_NOINLINE void Difference(const Solution &other) {
+  ABSL_ATTRIBUTE_NOINLINE void Combine(
+      const Solution &ABSL_RANDOM_INTERNAL_RESTRICT other) {
+    #pragma omp parallel for
     for (std::size_t index = 0; index < subsets.size(); ++index) {
       subsets.at(index).Join(other.subsets.at(subsets.size() - index - 1));
     }
+
     std::sort(subsets.begin(), subsets.end());
     difference = subsets.at(subsets.size() - 1).sum - subsets.at(0).sum;
   }
 
-  uint_fast64_t difference;
-  std::vector<Subset> subsets;
+  result_type difference;
+  std::vector<Subset<mapped_type, key_type, UnaryOp>> subsets;
 };
 
 }  // namespace
@@ -100,38 +130,62 @@ struct Solution {
 // `Computer-assisted proof of performance ratios for the Differencing Method
 // <https://www.sciencedirect.com/science/article/pii/S1572528611000508>`,
 // a variant of LDM for balanced number partitioning with larger cardinalities.
-ABSL_ATTRIBUTE_NOINLINE
-std::vector<std::pair<uint_fast64_t, std::vector<uint64_t>>> KarmarkarKarp(
-    const std::vector<std::pair<const uint16_t, uint64_t>> &items,
-    uint64_t micro_batch_size, uint64_t num_micro_batches) {
-  auto solutions = std::priority_queue<Solution>();
+template <typename Index, typename Size, typename UnaryOp>
+  requires(flatflow::data::internal::Unsigned<Index> &&
+           flatflow::data::internal::Unsigned<Size> &&
+           std::is_invocable_v<UnaryOp, Size>)
+ABSL_ATTRIBUTE_NOINLINE auto KarmarkarKarp(
+    const std::vector<std::pair<const Size, Index>> &ABSL_RANDOM_INTERNAL_RESTRICT items,
+    const Index &ABSL_RANDOM_INTERNAL_RESTRICT micro_batch_size,
+    const Index &ABSL_RANDOM_INTERNAL_RESTRICT num_micro_batches, UnaryOp op)
+    -> std::vector<std::pair<std::invoke_result_t<UnaryOp, Size>, std::vector<Index>>> {
+  CHECK_NE(micro_batch_size, 0);
+  CHECK_NE(num_micro_batches, 0);
+  CHECK_EQ(items.size() % micro_batch_size, 0);
+  CHECK_EQ(items.size() % num_micro_batches, 0);
+
+  const auto now = omp_get_wtime();
+
+  // Initially, BLDM starts with a sequence of `k` partial solutions, where each
+  // partial solution is obtained from the `m` smallest remaining items.
+  auto solutions = std::priority_queue<Solution<Index, Size, UnaryOp>>();
 
   for (std::size_t index = 0; index < items.size();
        index += static_cast<std::size_t>(num_micro_batches)) {
     solutions.emplace(std::span(items).subspan(
-        index, static_cast<std::size_t>(num_micro_batches)));
+                          index, static_cast<std::size_t>(num_micro_batches)),
+                      op);
   }
 
+  // Next, the algorithm selects two partial solutions from the sequence, for
+  // which the difference between the maximum and minimum subset sum is largest.
+  // These two solutions are combined into a new partial solution by joining the
+  // subset with the smallest sum in one solution with the subset with the
+  // largest sum in another solution, the subset with the second smallest sum in
+  // one solution with the subset with the second largest sum in another
+  // solution, and so on. This process is called differencing the solutions.
+  // The combined solution replaces the two solutions in the sequence, and we
+  // iterate this differencing operation until only one solution in the sequence
+  // remains, which is the balanced solution returned by BLDM.
   for (; 1 < solutions.size();) {
     auto solution = solutions.top();
     solutions.pop();
 
-    auto other = solutions.top();
+    solution.Combine(solutions.top());
     solutions.pop();
 
-    solution.Difference(other);
     solutions.emplace(solution);
   }
 
-  const auto &solution = solutions.top();
+  auto micro_batches = std::vector<
+      std::pair<std::invoke_result_t<UnaryOp, Size>, std::vector<Index>>>();
+  micro_batches.reserve(solutions.top().subsets.size());
 
-  auto micro_batches =
-      std::vector<std::pair<uint_fast64_t, std::vector<uint64_t>>>();
-  micro_batches.reserve(solution.subsets.size());
-
-  for (const auto &subset : solution.subsets) {
+  for (const auto &subset : solutions.top().subsets) {
     micro_batches.emplace_back(subset.sum, std::move(subset.indices));
   }
+
+  LOG(INFO) << absl::StrFormat("Partitioning %u items into %u subsets took %fs", items.size(), num_micro_batches, omp_get_wtime() - now);
 
   return micro_batches;
 }
