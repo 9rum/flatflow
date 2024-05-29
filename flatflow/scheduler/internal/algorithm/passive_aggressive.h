657ea6735e214a1000a8deacb3aa8721bd114fd5
9rum <sanha.maeng@kakao.com>
2024-05-29 12:23:27 +0900

    algorithm: refactor regressors

---
+++ flatflow/scheduler/internal/algorithm/passive_aggressive.h
@@ -0,0 +1,208 @@
+// Adapted from https://github.com/scikit-learn/scikit-learn/blob/1.5.0/sklearn/linear_model/_passive_aggressive.py
+// Copyright (c) 2007-2024 The scikit-learn developers. All rights reserved.
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
+#ifndef FLATFLOW_SCHEDULER_INTERNAL_ALGORITHM_PASSIVE_AGGRESSIVE_H_
+#define FLATFLOW_SCHEDULER_INTERNAL_ALGORITHM_PASSIVE_AGGRESSIVE_H_
+
+#include <algorithm>
+#include <array>
+#include <numeric>
+#include <vector>
+
+#include "absl/base/attributes.h"
+#include "absl/log/check.h"
+#include "absl/random/internal/platform.h"
+
+namespace flatflow {
+namespace scheduler {
+namespace internal {
+namespace algorithm {
+
+// PassiveAggressiveRegressor<>
+//
+// The passive-aggressive algorithms are a family of algorithms
+// for large-scale learning. They are similar to the perceptron
+// in that they do not require a learning rate. However, contrary
+// to the perceptron, they include a regularization parameter `C`.
+// See https://jmlr.csail.mit.edu/papers/volume7/crammer06a/crammer06a.pdf
+// for reference.
+//
+// This is a port of `sklearn.linear_model.PassiveAggressiveRegressor` for
+// internal usage. Note that it is not intended to be used for an arbitrary
+// number of features; this regressor can be used only for linear models.
+// A separate template specialization exists for quadratic models, and each
+// template specialization is optimized for the given order.
+template <std::size_t Order>
+class PassiveAggressiveRegressor {
+ public:
+  // `epsilon` denotes the threshold for prediction loss, which defaults to 0.1.
+  // If the difference between the current prediction and the correct label is
+  // below this threshold, the model is not updated.
+  inline ABSL_ATTRIBUTE_ALWAYS_INLINE explicit PassiveAggressiveRegressor(
+      double epsilon = 0.1, double C = 1.0, std::size_t max_iter = 1000)
+      : epsilon_(epsilon), C_(C), max_iter_(max_iter) {
+    coef_ = 1.0;
+    intercept_ = 0.0;
+  }
+
+  // PassiveAggressiveRegressor::fit()
+  //
+  // Fits the model with passive-aggressive algorithm.
+  // This uses epsilon-insensitive loss, which is equivalent to PA-I
+  // in the reference paper.
+  ABSL_ATTRIBUTE_NOINLINE bool fit(
+      const std::vector<double> &ABSL_RANDOM_INTERNAL_RESTRICT workloads,
+      const std::vector<double> &ABSL_RANDOM_INTERNAL_RESTRICT makespans) {
+    CHECK_EQ(workloads.size(), makespans.size());
+
+    auto converged = true;
+
+    for (std::size_t epoch = 0; epoch < max_iter_; ++epoch) {
+      for (std::size_t index = 0; index < workloads.size(); ++index) {
+        const auto workload = workloads[index];
+        const auto makespan = makespans[index];
+
+        const auto prediction = coef_ * workload + intercept_;
+        const auto loss = std::abs(makespan - prediction) - epsilon_;
+
+        if (0.0 < loss) {
+          const auto sqnorm = workload * (workload + 1.0);
+          const auto update = std::min(loss / sqnorm, C_);
+
+          if (prediction < makespan) {
+            coef_ += update * workload;
+            intercept_ += update;
+          } else {
+            coef_ -= update * workload;
+            intercept_ -= update;
+          }
+
+          converged = false;
+        }
+      }
+    }
+
+    return converged;
+  }
+
+  // PassiveAggressiveRegressor::predict()
+  //
+  // Predicts makespan for the given workload.
+  inline ABSL_ATTRIBUTE_ALWAYS_INLINE double predict(
+      double workload) const noexcept {
+    return coef_ * workload;
+  }
+
+  // PassiveAggressiveRegressor::intercept()
+  //
+  // Returns the current model intercept.
+  inline ABSL_ATTRIBUTE_ALWAYS_INLINE double intercept() const noexcept {
+    return intercept_;
+  }
+
+ protected:
+  std::size_t max_iter_;
+  double C_;
+  double coef_;
+  double epsilon_;
+  double intercept_;
+};
+
+// PassiveAggressiveRegressor<2>
+//
+// A template specialization of `PassiveAggressiveRegressor` for models with
+// quadratic complexity.
+template <>
+class PassiveAggressiveRegressor<2> {
+ public:
+  // Unlike its linear counterpart, this regressor requires `hidden_size`
+  // to initialize the coefficients since the complexity of Transformers is
+  // `O(n^2 d + n d^2)`, where `n` and `d` denote the sequence length and
+  // hidden size, respectively.
+  inline ABSL_ATTRIBUTE_ALWAYS_INLINE explicit PassiveAggressiveRegressor(
+      double hidden_size, double epsilon = 0.1, double C = 1.0,
+      std::size_t max_iter = 1000)
+      : epsilon_(epsilon), C_(C), max_iter_(max_iter) {
+    coef_ = std::to_array({1.0, hidden_size});
+    intercept_ = 0.0;
+  }
+
+  // PassiveAggressiveRegressor::fit()
+  //
+  // Fits the model with passive-aggressive algorithm. Unlike the linear model,
+  // this regressor takes one or more workloads for each makespan since multiple
+  // workloads are mapped to a single makespan in the profile. For this reason,
+  // a dot product-based regression is used instead of the canonical regression.
+  ABSL_ATTRIBUTE_NOINLINE bool fit(
+      const std::vector<std::vector<double>> &ABSL_RANDOM_INTERNAL_RESTRICT workloads,
+      const std::vector<double> &ABSL_RANDOM_INTERNAL_RESTRICT makespans) {
+    CHECK_EQ(workloads.size(), makespans.size());
+
+    auto converged = true;
+
+    for (std::size_t epoch = 0; epoch < max_iter_; ++epoch) {
+      for (std::size_t index = 0; index < workloads.size(); ++index) {
+        const auto &workload = workloads[index];
+        const auto makespan = makespans[index];
+
+        const auto sum =
+            std::accumulate(workload.cbegin(), workload.cend(), 0.0);
+        const auto sqsum = std::inner_product(
+            workload.cbegin(), workload.cend(), workload.cbegin(), 0.0);
+
+        const auto prediction = coef_[0] * sqsum + coef_[1] * sum + intercept_;
+        const auto loss = std::abs(makespan - prediction) - epsilon_;
+
+        if (0.0 < loss) {
+          const auto sqnorm = sum * sum + sqsum * sqsum;
+          const auto update = std::min(loss / sqnorm, C_);
+
+          if (prediction < makespan) {
+            coef_[0] += update * sqsum;
+            coef_[1] += update * sum;
+            intercept_ += update;
+          } else {
+            coef_[0] -= update * sqsum;
+            coef_[1] -= update * sum;
+            intercept_ -= update;
+          }
+
+          converged = false;
+        }
+      }
+    }
+
+    return converged;
+  }
+
+  // PassiveAggressiveRegressor::predict()
+  //
+  // Predicts makespan for the given workload.
+  inline ABSL_ATTRIBUTE_ALWAYS_INLINE double predict(
+      double workload) const noexcept {
+    return workload * (coef_[0] * workload + coef_[1]);
+  }
+
+  // PassiveAggressiveRegressor::intercept()
+  //
+  // Returns the current model intercept.
+  inline ABSL_ATTRIBUTE_ALWAYS_INLINE double intercept() const noexcept {
+    return intercept_;
+  }
+
+ protected:
+  std::size_t max_iter_;
+  double C_;
+  double epsilon_;
+  double intercept_;
+  std::array<double, 2> coef_;
+};
+
+}  // namespace algorithm
+}  // namespace internal
+}  // namespace scheduler
+}  // namespace flatflow
+
+#endif  // FLATFLOW_SCHEDULER_INTERNAL_ALGORITHM_PASSIVE_AGGRESSIVE_H_
---
+++ flatflow/scheduler/internal/algorithm/passive_aggressive_test.cc
@@ -0,0 +1,109 @@
+// Copyright 2024 The FlatFlow Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "flatflow/scheduler/internal/algorithm/passive_aggressive.h"
+
+#include <functional>
+#include <numeric>
+#include <random>
+#include <utility>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+namespace {
+
+class RegressorTest : public testing::Test {
+ protected:
+  void SetUp() override {
+    auto distribution = std::lognormal_distribution(5.252, 0.293);
+    auto generator = std::default_random_engine();
+
+    workloads.reserve(kNumMicroBatches);
+    while (workloads.size() < workloads.capacity()) {
+      auto workload = std::vector<double>();
+      workload.reserve(kMicroBatchSize);
+      while (workload.size() < workload.capacity()) {
+        workload.emplace_back(std::round(distribution(generator)));
+      }
+      workloads.emplace_back(std::move(workload));
+    }
+  }
+
+  static constexpr auto kMicroBatchSize = static_cast<std::size_t>(1 << 2);
+  static constexpr auto kNumMicroBatches = static_cast<std::size_t>(1 << 5);
+  static constexpr auto kEpsilon = 1e-4;
+  static constexpr auto kThreshold = 1e-2;
+  static constexpr auto kHiddenSize = 4e2;
+  std::vector<std::vector<double>> workloads;
+};
+
+TEST_F(RegressorTest, Linear) {
+  constexpr auto kCoefficient = 1e2;
+  constexpr auto kIntercept = 2e2;
+
+  auto sums = std::vector<double>();
+  sums.reserve(kNumMicroBatches);
+
+  auto makespans = std::vector<double>();
+  makespans.reserve(kNumMicroBatches);
+
+  for (const auto &workload : workloads) {
+    const auto sum = std::accumulate(workload.cbegin(), workload.cend(), 0.0);
+    sums.emplace_back(sum);
+    makespans.emplace_back(kCoefficient * sum + kIntercept);
+  }
+
+  auto regressor =
+      flatflow::scheduler::internal::algorithm::PassiveAggressiveRegressor<1>(
+          kEpsilon);
+  EXPECT_FALSE(regressor.fit(sums, makespans));
+
+  for (std::size_t index = 0; index < kNumMicroBatches; ++index) {
+    const auto sum = sums[index];
+    const auto makespan = makespans[index];
+    const auto prediction = regressor.predict(sum) + regressor.intercept();
+    EXPECT_LE(std::abs(makespan - prediction) / makespan, kThreshold);
+  }
+}
+
+TEST_F(RegressorTest, Quadratic) {
+  constexpr auto kIntercept = 3e2;
+
+  auto makespans = std::vector<double>();
+  makespans.reserve(kNumMicroBatches);
+
+  for (const auto &workload : workloads) {
+    const auto sum = std::accumulate(workload.cbegin(), workload.cend(), 0.0);
+    const auto sqsum = std::inner_product(workload.cbegin(), workload.cend(),
+                                          workload.cbegin(), 0.0);
+    makespans.emplace_back(sqsum + kHiddenSize * sum + kIntercept);
+  }
+
+  auto regressor =
+      flatflow::scheduler::internal::algorithm::PassiveAggressiveRegressor<2>(
+          kHiddenSize, kEpsilon);
+  EXPECT_FALSE(regressor.fit(workloads, makespans));
+
+  for (std::size_t index = 0; index < kNumMicroBatches; ++index) {
+    const auto &workload = workloads[index];
+    const auto makespan = makespans[index];
+    const auto prediction = std::transform_reduce(
+        workload.cbegin(), workload.cend(), regressor.intercept(), std::plus(),
+        [&](const auto size) { return regressor.predict(size); });
+    EXPECT_LE(std::abs(makespan - prediction) / makespan, kThreshold);
+  }
+}
+
+}  // namespace
