fe1b7051903b0f3ab4130925fd29599a385d448e
Lee Hyun Joon <andylee70044@gmail.com>
2024-07-15 19:51:56 +0900

    modules: inherit nn.Linear

--- flatflow/torch/nn/modules/linear.py
+++ flatflow/torch/nn/modules/linear.py
@@ -1,15 +1,10 @@
-import math
 from typing import Dict
 
 import torch
-from torch import Tensor
-from torch.nn.parameter import Parameter
-from .. import functional as F
-from .. import init
-from .module import Module
+from torch import Tensor, nn
 
 
-class Linear(Module):
+class Linear(nn.Linear):
     r"""Custom Linear layer for flatflow.
     Offsets are required to handle variable length inputs.
     Applies a linear transformation to the incoming data: :math:`y = xA^T + b`.
@@ -50,11 +45,6 @@ class Linear(Module):
         torch.Size([128, 30])
     """
 
-    __constants__ = ["in_features", "out_features"]
-    in_features: int
-    out_features: int
-    weight: Tensor
-
     def __init__(
         self,
         in_features: int,
@@ -63,36 +53,15 @@ class Linear(Module):
         device=None,
         dtype=None,
     ) -> None:
-        factory_kwargs = {"device": device, "dtype": dtype}
-        super().__init__()
-        self.in_features = in_features
-        self.out_features = out_features
-        self.weight = Parameter(
-            torch.empty((out_features, in_features), **factory_kwargs)
-        )
-        if bias:
-            self.bias = Parameter(torch.empty(out_features, **factory_kwargs))
-        else:
-            self.register_parameter("bias", None)
-        self.reset_parameters()
+        super().__init__(in_features, out_features, bias, device, dtype)
 
-    def reset_parameters(self) -> None:
-        # Setting a=sqrt(5) in kaiming_uniform is the same as initializing with
-        # uniform(-1/sqrt(in_features), 1/sqrt(in_features)). For details, see
-        # https://github.com/pytorch/pytorch/issues/57109
-        init.kaiming_uniform_(self.weight, a=math.sqrt(5))
-        if self.bias is not None:
-            fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
-            bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
-            init.uniform_(self.bias, -bound, bound)
-
-    def forward(self, input: Tensor, offsets: Dict) -> Tensor:
+    def forward(self, input_tensor: Tensor, offsets: Dict) -> Tensor:
         """
+        Args:
+        input_tensor : torch.Tensor
+            Input tensor to be passed to the forward method of the Linear layer
         offsets : Dict[str, Union[List[Any], int]]
-        Offset values of cu_seqlens and max_seqlen
-        Consist of four keys : cu_seqlens_q, cu_seqlens_k, max_seqlen_q, max_seqlen_k
+            Offset values of cu_seqlens and max_seqlen
+            Consist of four keys : cu_seqlens_q, cu_seqlens_k, max_seqlen_q, max_seqlen_k
         """
-        return F.linear(input, self.weight, self.bias), offsets
-
-    def extra_repr(self) -> str:
-        return f"in_features={self.in_features}, out_features={self.out_features}, bias={self.bias is not None}"
+        return super().forward(input_tensor), offsets
