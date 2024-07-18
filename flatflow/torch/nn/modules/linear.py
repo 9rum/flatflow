5935cd66ee860bdc0576028dd2dc8e1eea5b950e
9rum <sanha.maeng@kakao.com>
2024-07-18 14:47:12 +0900

    modules: refactor linear layer

--- flatflow/torch/nn/modules/linear.py
+++ flatflow/torch/nn/modules/linear.py
@@ -1,70 +1,36 @@
-from typing import List
+# Adapted from https://github.com/pytorch/pytorch/blob/v2.3.1/torch/nn/modules/linear.py
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from collections.abc import Sequence
 
 import torch
-from torch import Tensor, nn
+from torch import Tensor
+
+__all__ = ["Linear"]
 
 
-class Linear(nn.Linear):
-    r"""Custom Linear layer for flatflow.
-    Offsets are required to handle variable length inputs.
-    Applies a linear transformation to the incoming data: :math:`y = xA^T + b`.
+class Linear(torch.nn.Linear):
+    """Applies a linear transformation to the incoming data: :math:`y = xA^T + b`.
 
-    This module supports :ref:`TensorFloat32<tf32_on_ampere>`.
+    This module is a port of :class:`torch.nn.Linear` for use in conjunction with the concatenation-based batching.
 
-    On certain ROCm devices, when using float16 inputs this module will use :ref:`different precision<fp16_on_mi200>` for backward.
+    One API difference is that :param:`offsets` is given as an additional argument to :meth:`forward`.
+    Note that :param:`offsets` is not used directly in this module since linear transformations do not
+    require any notion of data samples.
+
+    Another API difference is that :meth:`forward` returns :param:`offsets` as an additional return value.
+    This is to propagate :param:`offsets` in case the downstream layers require the notion of data samples.
 
     Args:
-        in_features: size of each input sample
-        out_features: size of each output sample
-        bias: If set to ``False``, the layer will not learn an additive bias.
-            Default: ``True``
-
-    Shape:
-        - Input: :math:`(*, H_{in})` where :math:`*` means any number of
-          dimensions including none and :math:`H_{in} = \text{in\_features}`.
-        - Output: :math:`(*, H_{out})` where all but the last dimension
-          are the same shape as the input and :math:`H_{out} = \text{out\_features}`.
-
-    Attributes:
-        weight: the learnable weights of the module of shape
-            :math:`(\text{out\_features}, \text{in\_features})`. The values are
-            initialized from :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})`, where
-            :math:`k = \frac{1}{\text{in\_features}}`
-        bias:   the learnable bias of the module of shape :math:`(\text{out\_features})`.
-                If :attr:`bias` is ``True``, the values are initialized from
-                :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})` where
-                :math:`k = \frac{1}{\text{in\_features}}`
-
-    Examples::
-
-        >>> m = nn.Linear(20, 30)
-        >>> input = torch.randn(128, 20)
-        >>> offsets = [0, 2, 3, 6] # offset where to slice inputs
-        >>> output = m(input, offsets)
-        >>> print(output.size())
-        torch.Size([128, 30])
+        in_features (int): size of each input sample
+        out_features (int): size of each output sample
+        bias (bool): If ``False``, the layer will not learn an additive bias (default: ``True``).
     """
 
-    def __init__(
-        self,
-        in_features: int,
-        out_features: int,
-        bias: bool = True,
-        device=None,
-        dtype=None,
-    ) -> None:
+    def __init__(self, in_features: int, out_features: int, bias: bool = True, device=None, dtype=None) -> None:
         super().__init__(in_features, out_features, bias, device, dtype)
 
-    def forward(
-        self,
-        input: Tensor,
-        offsets: List[int],
-    ) -> tuple[Tensor, List[int]]:
-        """
-        Args:
-        input : torch.Tensor
-            Input tensor to be passed to the forward method of the Linear layer
-        offsets : List[int]
-            List of offsets to handle variable length inputs
-        """
-        return (super().forward(input), offsets)
+    def forward(self, input: Tensor, offsets: Sequence[int]) -> tuple[Tensor, Sequence[int]]:
+        return super().forward(input), offsets
