1edbf61b1d11cddbd0fe2b87cb06794efb80c271
9rum <sanha.maeng@kakao.com>
2024-07-29 14:44:30 +0900

    data: improve compatibility with PyTorch data set

--- flatflow/torch/utils/data/dataset.py
+++ flatflow/torch/utils/data/dataset.py
@@ -10,6 +10,8 @@ from typing import TypeVar
 
 import torch
 
+from flatflow import sys
+
 __all__ = [
     "ChainDataset",
     "ConcatDataset",
@@ -30,7 +32,7 @@ class Dataset(torch.utils.data.Dataset[T_co]):
     the user-defined size of the data sample at position :param:`index`.
     """
 
-    def __add__(self, other: "Dataset[T_co]") -> "ConcatDataset[T_co]":
+    def __add__(self, other: torch.utils.data.Dataset[T_co]) -> "ConcatDataset[T_co]":
         return ConcatDataset([self, other])
 
     def __sizeof__(self, index: int) -> int:
@@ -57,7 +59,7 @@ class IterableDataset(Dataset[T_co], torch.utils.data.IterableDataset[T_co]):
     to modify each copy's behavior.
     """
 
-    def __add__(self, other: Dataset[T_co]):
+    def __add__(self, other: torch.utils.data.Dataset[T_co]):
         return ChainDataset([self, other])
 
 
@@ -70,22 +72,24 @@ class ConcatDataset(Dataset[T_co]):
         datasets (Iterable[Dataset]): List of data sets to be concatenated.
     """
 
-    datasets: Sequence[Dataset[T_co]]
+    datasets: Sequence[torch.utils.data.Dataset[T_co]]
     cumulative_sizes: Sequence[int]
 
     @staticmethod
-    def cumsum(sequence: Sequence[Dataset]) -> Sequence[int]:
+    def cumsum(sequence: Sequence[torch.utils.data.Dataset]) -> Sequence[int]:
         r = [0] * len(sequence)
         for i, e in enumerate(sequence):
             r[i] = r[i - 1] + len(e)  # type: ignore[arg-type]
         return r
 
-    def __init__(self, datasets: Iterable[Dataset]) -> None:
+    def __init__(self, datasets: Iterable[torch.utils.data.Dataset]) -> None:
         super().__init__()
         self.datasets = list(datasets)
-        assert 0 < len(self.datasets), "datasets should not be an empty iterable"
+        assert 0 < len(self.datasets), "datasets should not be an empty iterable."
         for d in self.datasets:
-            assert not isinstance(d, IterableDataset), "ConcatDataset does not support IterableDataset"
+            assert not isinstance(
+                d, torch.utils.data.IterableDataset
+            ), "ConcatDataset does not support IterableDataset."
         self.cumulative_sizes = self.cumsum(self.datasets)
 
     def __len__(self) -> int:
@@ -93,8 +97,8 @@ class ConcatDataset(Dataset[T_co]):
 
     def __getitem__(self, idx: int) -> T_co:
         if idx < 0:
-            if len(self) < -idx:
-                raise ValueError("absolute value of index should not exceed data set length")
+            if len(self) + idx < 0:
+                raise ValueError("absolute value of index should not exceed data set length.")
             idx += len(self)
         dataset_idx = bisect.bisect_right(self.cumulative_sizes, idx)
         if dataset_idx == 0:
@@ -103,9 +107,21 @@ class ConcatDataset(Dataset[T_co]):
             sample_idx = idx - self.cumulative_sizes[dataset_idx - 1]
         return self.datasets[dataset_idx][sample_idx]
 
+    def __sizeof__(self, idx: int) -> int:
+        if idx < 0:
+            if len(self) + idx < 0:
+                raise ValueError("absolute value of index should not exceed data set length.")
+            idx += len(self)
+        dataset_idx = bisect.bisect_right(self.cumulative_sizes, idx)
+        if dataset_idx == 0:
+            sample_idx = idx
+        else:
+            sample_idx = idx - self.cumulative_sizes[dataset_idx - 1]
+        return sys.getsizeof(self.datasets[dataset_idx], sample_idx)
+
     @property
     def cummulative_sizes(self) -> Sequence[int]:
-        warnings.warn("cummulative_sizes attribute is renamed to cumulative_sizes", DeprecationWarning, 2)
+        warnings.warn("cummulative_sizes attribute is renamed to cumulative_sizes.", DeprecationWarning, 2)
         return self.cumulative_sizes
 
 
@@ -120,18 +136,18 @@ class ChainDataset(IterableDataset):
         datasets (Iterable[Dataset]): data sets to be chained together
     """
 
-    def __init__(self, datasets: Iterable[Dataset]) -> None:
+    def __init__(self, datasets: Iterable[torch.utils.data.Dataset]) -> None:
         super().__init__()
         self.datasets = datasets
 
     def __iter__(self):
         for d in self.datasets:
-            assert isinstance(d, IterableDataset), "ChainDataset only supports IterableDataset"
+            assert isinstance(d, torch.utils.data.IterableDataset), "ChainDataset only supports IterableDataset."
             yield from d
 
     def __len__(self) -> int:
         total = 0
         for d in self.datasets:
-            assert isinstance(d, IterableDataset), "ChainDataset only supports IterableDataset"
+            assert isinstance(d, torch.utils.data.IterableDataset), "ChainDataset only supports IterableDataset."
             total += len(d)  # type: ignore[arg-type]
         return total
