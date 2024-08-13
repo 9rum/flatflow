93888e3443cac0736f2a9257943947d4a0f4d289
9rum <sanha.maeng@kakao.com>
2024-08-13 13:06:19 +0900

    rpc: implement shutdown protocol

--- flatflow/rpc/communicator.h
+++ flatflow/rpc/communicator.h
@@ -16,20 +16,14 @@
 #define FLATFLOW_RPC_COMMUNICATOR_H_
 
 #include <atomic>
-#include <csignal>
 #include <cstdint>
 #include <future>
 #include <utility>
 #include <variant>
 #include <vector>
 
-#include "absl/base/log_severity.h"
-#include "absl/log/globals.h"
-#include "absl/log/initialize.h"
-#include "absl/log/internal/globals.h"
 #include "absl/log/log.h"
 #include "absl/strings/str_format.h"
-#include "grpcpp/grpcpp.h"
 
 #include "flatflow/rpc/communicator.grpc.fb.h"
 #include "flatflow/rpc/communicator_generated.h"
@@ -55,19 +49,22 @@ class CommunicatorServiceImpl final : public Communicator::Service {
   // Constructors and assignment operators
   //
   // There are only default constructors and assignment operators to allow copy
-  // elision, except for passing the data parallel size to synchronize workers
+  // elision, except for opening communication channels to synchronize workers
   // upon initialization. The actual initializations are handled through `Init`.
   explicit CommunicatorServiceImpl() {}
 
-  explicit CommunicatorServiceImpl(uint64_t data_parallel_size)
-      : data_parallel_size_(data_parallel_size) {
-    sync_ = 0;
+  explicit CommunicatorServiceImpl(uint64_t data_parallel_size,
+                                   std::promise<void> handler)
+      : data_parallel_size_(data_parallel_size), handler_(std::move(handler)) {
+    fanin_ = 0;
 
     const auto _data_parallel_size =
         static_cast<std::size_t>(data_parallel_size);
+    fanout_.first.reserve(_data_parallel_size);
+    fanout_.second.reserve(_data_parallel_size);
     for (std::size_t rank = 0; rank < _data_parallel_size; ++rank) {
-      producers_[rank] = std::promise<std::vector<uint64_t>>();
-      consumers_[rank] = producers_[rank].get_future();
+      fanout_.first.emplace_back();
+      fanout_.second.emplace_back(std::move(fanout_.first[rank].get_future()));
     }
   }
 
@@ -89,7 +86,7 @@ class CommunicatorServiceImpl final : public Communicator::Service {
     const auto rank = static_cast<std::size_t>(args->rank());
 
     LOG(INFO) << absl::StrFormat("Init called from %s (rank %u)", context->peer(), rank);
-    ++sync_;
+    ++fanin_;
 
     if (rank == 0) {
       const auto order = args->order();
@@ -127,11 +124,11 @@ class CommunicatorServiceImpl final : public Communicator::Service {
       // when compare-and-swap is inside a loop.
       // See https://en.cppreference.com/w/cpp/atomic/atomic/compare_exchange.
       auto expected = data_parallel_size_;
-      while (!sync_.compare_exchange_weak(expected, 0)) {
+      while (!fanin_.compare_exchange_weak(expected, 0)) {
         expected = data_parallel_size_;
       }
 
-      for (auto &producer : producers_) {
+      for (auto &producer : fanout_.first) {
         producer.set_value(std::move(std::vector<uint64_t>()));
       }
     }
@@ -140,12 +137,12 @@ class CommunicatorServiceImpl final : public Communicator::Service {
     builder_.Finish(offset);
     *response = builder_.ReleaseMessage<Empty>();
 
-    consumers_[rank].get();
+    fanout_.second[rank].get();
 
     // The promise-future communication channel is disposable; each worker
     // should reset its own channel after receiving a fanout signal.
-    producers_[rank] = std::promise<std::vector<uint64_t>>();
-    consumers_[rank] = producers_[rank].get_future();
+    fanout_.first[rank] = std::promise<std::vector<uint64_t>>();
+    fanout_.second[rank] = fanout_.first[rank].get_future();
 
     return grpc::Status::OK;
   }
@@ -163,19 +160,19 @@ class CommunicatorServiceImpl final : public Communicator::Service {
     const auto rank = static_cast<std::size_t>(args->rank());
 
     LOG(INFO) << absl::StrFormat("Broadcast called from %s (rank %u)", context->peer(), rank);
-    ++sync_;
+    ++fanin_;
 
     if (rank == 0) {
       auto expected = data_parallel_size_;
-      while (!sync_.compare_exchange_weak(expected, 0)) {
+      while (!fanin_.compare_exchange_weak(expected, 0)) {
         expected = data_parallel_size_;
       }
     }
 
-    const auto indices = consumers_[rank].get();
+    const auto indices = fanout_.second[rank].get();
 
-    producers_[rank] = std::promise<std::vector<uint64_t>>();
-    consumers_[rank] = producers_[rank].get_future();
+    fanout_.first[rank] = std::promise<std::vector<uint64_t>>();
+    fanout_.second[rank] = fanout_.first[rank].get_future();
 
     return grpc::Status::OK;
   }
@@ -188,7 +185,7 @@ class CommunicatorServiceImpl final : public Communicator::Service {
                         flatbuffers::grpc::Message<Empty> *response) override {
     LOG(INFO) << absl::StrFormat("Finalize called from %s", context->peer());
 
-    std::raise(SIGTERM);
+    handler_.set_value();
 
     auto offset = CreateEmpty(builder_);
     builder_.Finish(offset);
@@ -201,45 +198,17 @@ class CommunicatorServiceImpl final : public Communicator::Service {
   uint64_t data_parallel_size_;
   uint64_t batch_;
   uint64_t epoch_;
-  std::atomic_uint64_t sync_;
-  std::vector<std::promise<std::vector<uint64_t>>> producers_;
-  std::vector<std::future<std::vector<uint64_t>>> consumers_;
+  std::atomic_uint64_t fanin_;
+  std::promise<void> handler_;
+  std::pair<std::vector<std::promise<std::vector<uint64_t>>>,
+            std::vector<std::future<std::vector<uint64_t>>>>
+      fanout_;
   flatbuffers::grpc::MessageBuilder builder_;
   std::variant<std::monostate,
                flatflow::scheduler::Scheduler<uint64_t, uint16_t, 1, false>,
                flatflow::scheduler::Scheduler<uint64_t, uint16_t, 2, false>>
       scheduler_;
 };
-
-// flatflow::rpc::run()
-//
-// Executes the communicator runtime. This routine is invoked from the Python
-// frontend via foreign function interface (FFI); that is, there is no direct
-// entry point to the communicator runtime and the actual initialization and
-// termination are handled through `Init` and `Finalize`, respectively.
-void run(uint16_t port, uint64_t data_parallel_size) {
-  if (!absl::log_internal::IsInitialized()) {
-    absl::InitializeLog();
-    absl::SetStderrThreshold(absl::LogSeverity::kInfo);
-  }
-
-  auto builder = grpc::ServerBuilder();
-  auto addr = absl::StrFormat("[::]:%u", port);
-  builder.AddListeningPort(addr, grpc::InsecureServerCredentials());
-
-  auto service = CommunicatorServiceImpl(data_parallel_size);
-  builder.RegisterService(&service);
-
-  static auto server = builder.BuildAndStart();
-  if (server == nullptr) {
-    LOG(FATAL) << absl::StrFormat("Failed to start server on %s", addr);
-  }
-
-  auto shutdown = [](int signal) { server->Shutdown(); };
-  std::signal(SIGTERM, shutdown);
-
-  LOG(INFO) << absl::StrFormat("Server listening on %s", addr);
-}
 
 }  // namespace rpc
 }  // namespace flatflow
