28e6ee1fc82c89a45f7048fc9475e6f09850f47c
Lee Hyun Joon <andylee70044@gmail.com>
2024-09-10 15:55:05 +0900

    data: implement distributed sampler

--- flatflow/torch/utils/data/distributed.py
+++ flatflow/torch/utils/data/distributed.py
@@ -1,4 +1,5 @@
 # Adapted from https://github.com/pytorch/pytorch/blob/v2.4.0/torch/utils/data/distributed.py
+import math
 import os
 import shutil
 import subprocess
@@ -22,48 +23,75 @@ __all__ = ["DistributedSampler"]
 T_co = TypeVar('T_co', covariant=True)
 
 class DistributedSampler(Sampler[T_co]):
-  def __init__(self, dataset: Dataset, global_batch_size: int, micro_batch_size: int,
+  def __init__(self, dataset: Dataset, global_batch_size: int, micro_batch_size: int, order: Optional[int] = 2,
                rank: Optional[int] = None, use_flat_shuffle: bool = True,
                seed: int = 0, master_addr: Optional[str] = None,
                master_port: int = 50051, heterogeneous: bool = None,
                hidden_size: int = False, sizes: Optional[Sequence[int]] = None) -> None:
+    
+    if num_replicas is None:
+      if not dist.is_available():
+        raise RuntimeError("Requires distributed package to be available")
+      num_replicas = dist.get_world_size()
+    if rank is None:
+      if not dist.is_available():
+        raise RuntimeError("Requires distributed package to be available")
+      rank = dist.get_rank()
+    if master_addr is None:
+      master_addr = os.getenv("MASTER_ADDR")
+      if master_addr is None:
+        raise ValueError("Invalid master address {}, either master address or MASTER_ADDR should be given".format(master_addr))
+    
+    self.num_replicas = num_replicas
+    self.dataset = dataset
+    self.rank = rank
+    self.map = list(range(len(self.dataset)))  # type: ignore[arg-type]
+    self.num_samples = math.ceil(len(self.dataset) / self.num_replicas)
+    
+    if self.rank == 0:
+      data_parallel_size = torch.distributed.get_world_size()
+      run(master_port, data_parallel_size)
+    
     addr = os.getenv("MASTER_ADDR", master_addr)
     channel = grpc.insecure_channel(f"{addr}:{master_port}")
     self.stub = CommunicatorClient(channel)
-    self.stub.Init(global_batch_size, micro_batch_size, rank, seed, heterogeneous, use_flat_shuffle, hidden_size, sizes)
+    self.stub.Init(global_batch_size, micro_batch_size, order, rank, seed, heterogeneous, use_flat_shuffle, hidden_size, sizes)
     
-    if rank == 0:
-      data_parallel_size = torch.distributed.get_world_size()
-      run(master_port, data_parallel_size)
 
   def __iter__(self) -> Iterator[T_co]:
-      broadcast = self.stub.Broadcast(epoch=self.epoch)  # type: ignore[arg-type]
-      self.indices = [broadcast.Indices(i) for i in range(broadcast.IndicesLength())]
-
-      self._num_yielded = 0
-      return self
+    broadcast = self.stub.Broadcast(epoch=self.epoch)  # type: ignore[arg-type]
+    self.indices = [broadcast.Indices(i) for i in range(broadcast.IndicesLength())]
+    self._num_yielded = 0
+    return self
   
   def __next__(self) -> T_co:
     if self.num_samples <= self._num_yielded:
-        raise StopIteration
-    index = self.map[self.indices[self._num_yielded]] #todo
+      raise StopIteration
+    index = self.map[self.indices[self._num_yielded]]
 
     self._num_yielded += 1
     return index
+  
+  def __len__(self) -> int:
+    """Length of Batch Sampler.
 
+    ..note::
+        When `rampup_batch_size` is enabled, the return value can be not exactly precise.
+
+    """
+    num_available_samples: int = self.num_samples - self._num_yielded
+    return (num_available_samples + self.global_batch_size - 1) // self.global_batch_size
+
+  
   def __del__(self) -> None:
     if self.rank == 0:
         self.client.Finalize()
 
+  def set_epoch(self, epoch: int) -> None:
+      r"""Sets the epoch for this sampler. This ensures all replicas use a different random ordering for each epoch.
+      Otherwise, the next iteration of this sampler will yield the same ordering.
 
-if __name__ == "__main__":
-    dataset = torch.utils.data.TensorDataset(torch.rand(100, 2), torch.rand(100, 1))
-    sampler = DistributedSampler(dataset, global_batch_size=32, micro_batch_size=16, rank=0)
-    dataloader = torch.utils.data.DataLoader(dataset, sampler=sampler, batch_size=None)
-    for data in dataloader:
-        print(data)
-        break
-    del sampler
-    del dataloader
-    del dataset
-    print("Done")
+      Args:
+          epoch (int): Epoch number.
+      """
+      self.epoch = epoch
