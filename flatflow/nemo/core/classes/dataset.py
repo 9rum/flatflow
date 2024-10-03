5e26ca7613c1d6c0fc14c823c1b85b982fb50654
9rum <sanha.maeng@kakao.com>
2024-10-03 19:08:46 +0900

    classes: make NeMo data sets use concatenation-based batching

--- flatflow/nemo/core/classes/dataset.py
+++ flatflow/nemo/core/classes/dataset.py
@@ -14,102 +14,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from dataclasses import dataclass
-from typing import Optional
+import nemo.core.classes
 
-from torch.utils import data
+import flatflow.torch.utils.data
 
-from nemo.core.classes import Serialization, Typing, typecheck
+__all__ = [
+    "Dataset",
+    "IterableDataset",
+]
 
-__all__ = ['Dataset', 'IterableDataset']
 
+class Dataset(flatflow.torch.utils.data.Dataset, nemo.core.classes.Dataset):
+    """Data set with output ports.
 
-class Dataset(data.Dataset, Typing, Serialization):
-    """Dataset with output ports
-
-    Please Note: Subclasses of IterableDataset should *not* implement input_types.
+    .. note::
+        Subclasses of :class:`~flatflow.nemo.core.classes.Dataset` should *not* implement :attr:`input_types`.
     """
 
     def _collate_fn(self, batch):
-        """
-        A default implementation of a collation function.
-        Users should override this method to define custom data loaders.
-        """
-        return data.dataloader.default_collate(batch)
-
-    @typecheck()
-    def collate_fn(self, batch):
-        """
-        This is the method that user pass as functor to DataLoader.
-        The method optionally performs neural type checking and add types to the outputs.
-
-        Please note, subclasses of Dataset should not implement `input_types`.
-
-        Usage:
-
-        .. code-block:: python
-
-            dataloader = torch.utils.data.DataLoader(
-                    ....,
-                    collate_fn=dataset.collate_fn,
-                    ....
-            )
-
-        Returns:
-            Collated batch, with or without types.
-        """
-        if self.input_types is not None:
-            raise TypeError("Datasets should not implement `input_types` as they are not checked")
-
-        # Simply forward the inner `_collate_fn`
-        return self._collate_fn(batch)
+        return flatflow.torch.utils.data.default_collate(batch)
 
 
-class IterableDataset(data.IterableDataset, Typing, Serialization):
-    """Iterable Dataset with output ports
+class IterableDataset(flatflow.torch.utils.data.IterableDataset, nemo.core.classes.IterableDataset):
+    """Iterable data set with output ports.
 
-    Please Note: Subclasses of IterableDataset should *not* implement input_types.
+    .. note::
+        Subclasses of :class:`~flatflow.nemo.core.classes.IterableDataset` should *not* implement :attr:`input_types`.
     """
 
     def _collate_fn(self, batch):
-        """
-        A default implementation of a collation function.
-        Users should override this method to define custom data loaders.
-        """
-        return data.dataloader.default_collate(batch)
-
-    @typecheck()
-    def collate_fn(self, batch):
-        """
-        This is the method that user pass as functor to DataLoader.
-        The method optionally performs neural type checking and add types to the outputs.
-
-        # Usage:
-        dataloader = torch.utils.data.DataLoader(
-                ....,
-                collate_fn=dataset.collate_fn,
-                ....
-        )
-
-        Returns:
-            Collated batch, with or without types.
-        """
-        if self.input_types is not None:
-            raise TypeError("Datasets should not implement `input_types` as they are not checked")
-
-        # Simply forward the inner `_collate_fn`
-        return self._collate_fn(batch)
-
-
-@dataclass
-class DatasetConfig:
-    """
-
-    """
-
-    # ...
-    batch_size: int = 32
-    drop_last: bool = False
-    shuffle: bool = False
-    num_workers: Optional[int] = 0
-    pin_memory: bool = True
+        return flatflow.torch.utils.data.default_collate(batch)
