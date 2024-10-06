d5c00fccbe576195bdba4386a2863bca15ae3f37
Lee Hyun Joon <andylee70044@gmail.com>
2024-10-06 15:08:00 +0900

    megatron: add MegatronPretrainingBatchSampler in init language_modeling: apply flatflow sampler

--- flatflow/nemo/collections/nlp/data/language_modeling/megatron/__init__.py
+++ flatflow/nemo/collections/nlp/data/language_modeling/megatron/__init__.py
@@ -1,3 +1,9 @@
 from flatflow.nemo.collections.nlp.data.language_modeling.megatron.gpt_sft_dataset import GPTSFTDataset
+from flatflow.nemo.collections.nlp.data.language_modeling.megatron.megatron_batch_samplers import (
+    MegatronPretrainingBatchSampler,
+)
 
-__all__ = ["GPTSFTDataset"]
+__all__ = [
+    "GPTSFTDataset",
+    "MegatronPretrainingBatchSampler",
+]
