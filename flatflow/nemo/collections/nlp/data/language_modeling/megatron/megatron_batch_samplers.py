8bc280fa3a7c954a35ee4955accbac646bfdb070
Lee Hyun Joon <andylee70044@gmail.com>
2024-11-26 11:59:31 +0900

    megatron: fix typos

--- flatflow/nemo/collections/nlp/data/language_modeling/megatron/megatron_batch_samplers.py
+++ flatflow/nemo/collections/nlp/data/language_modeling/megatron/megatron_batch_samplers.py
@@ -149,16 +149,17 @@ class MegatronPretrainingBatchSampler(BaseMegatronBatchSampler):
             )
 
     @staticmethod
-    def get_model_parallel_src_rank():
-        """Calculate the global rank corresponding to the first local rank
-        in the model parallel group."""
+    def get_model_parallel_src_rank() -> int:
+        r"""Calculate the global rank corresponding to the first local rank
+        in the model parallel group.
+        """
         world_size = torch.distributed.get_world_size()
-        all_ranks = np.arange(world_size)
-        tp_size = parallel_state.get_tensor_model_parallel_world_size()
-        pp_size = parallel_state.get_pipeline_model_parallel_world_size()
-        all_ranks = all_ranks.reshape(pp_size, -1, tp_size)
-        dp_rank = parallel_state.get_data_parallel_rank()
-        return all_ranks[:, dp_rank, :].min()
+        total_rank = np.arange(world_size)
+        pipeline_parallel_size = parallel_state.get_pipeline_model_parallel_world_size()
+        tensor_parallel_size = parallel_state.get_tensor_model_parallel_world_size()
+        total_rank = total_rank.reshape(pipeline_parallel_size, -1, tensor_parallel_size)
+        data_parallel_rank = parallel_state.get_data_parallel_rank()
+        return total_rank[:, data_parallel_rank, :].min()
 
     def set_epoch(self, epoch: int) -> None:
         r"""Sets the epoch for this sampler. This ensures all replicas use a different random ordering for each epoch.
