ef408a0ac33ef0c0a5ce841912aa74d56a11f41f
amo33 <andylee70044@gmail.com>
2024-11-27 22:15:06 +0900

    core: apply ruff

--- flatflow/megatron/core/__init__.py
+++ flatflow/megatron/core/__init__.py
@@ -1,6 +1,4 @@
-from flatflow.megatron.core.parallel_state import (
-    get_model_parallel_src_rank
-)
+from flatflow.megatron.core.parallel_state import get_model_parallel_src_rank
 
 __all__ = [
     "get_model_parallel_src_rank",
