7ab4049018079c85df7a71953fc9dec5998da788
9rum <sanha.maeng@kakao.com>
2025-01-29 22:04:39 +0900

    internal: refactor inter-batch shuffling

--- flatflow/scheduler/internal/shuffle.h
+++ flatflow/scheduler/internal/shuffle.h
@@ -16,99 +16,67 @@
 #define FLATFLOW_SCHEDULER_INTERNAL_SHUFFLE_H_
 
 #include <algorithm>
-#include <cassert>
 #include <execution>
 #include <iterator>
-#include <numeric>
+#include <random>
 #include <utility>
 #include <vector>
 
-#include "flatflow/aten/generator.h"
 #include "flatflow/data/internal/types.h"
+#include "flatflow/scheduler/internal/partition.h"
 
 namespace flatflow {
 namespace internal {
 
 // shuffle()
 //
-// After scheduling, a `flatflow::Scheduler<>` shuffles between batches, which
+// After partitioning, a `flatflow::Scheduler<>` shuffles between batches, which
 // we call inter-batch shuffling. This enables shuffling not only between data
-// samples with the same size but also between scheduled batches. It uses the
+// samples with the same size but also between scheduled batches. We use the
 // same pseudo-random number generator and random seed as `flatflow::Dataset<>`
 // for deterministic shuffling.
-template <typename R, typename Index, typename Size>
-  requires(Numerical<R> && Unsigned<Index> && Unsigned<Size>)
-std::vector<std::vector<std::pair<Size, Index>>> shuffle(
-    const std::vector<std::pair<R, std::vector<std::pair<Size, Index>>>> &micro_batches,
-    Index seed, bool use_flat_shuffle) {
-  const auto num_micro_batches = micro_batches.size();
-  assert(num_micro_batches != 0);
+template <typename Index, typename R>
+  requires(Unsigned<Index> && Numerical<R>)
+void shuffle(std::vector<Subset<Index, R>> &subsets, const Index &seed) {
+  using iterator = typename std::vector<Subset<Index, R>>::iterator;
 
-  // The inter-batch shuffling is carried out as follows:
-  //
-  // * First, group micro-batches with the same makespan to minimize computation
-  //   stall. This forms shuffling ranges.
-  // * Second, shuffle between these shuffling ranges if the user wants the data
-  //   samples to be more shuffled; i.e., when `use_flat_shuffle` is set to
-  //   `false`.
-  // * Finally, shuffle the micro-batches within each shuffling range.
-  //   To minimize memory movement, shuffle the indices first and then project
-  //   the micro-batches onto the shuffled indices. Note that shuffling indices
-  //   first and then mapping the data to the shuffled indices yields the same
-  //   result as directly shuffling the data.
+  if (subsets.empty()) {
+    return;
+  }
+
+  const auto num_subsets = subsets.size();
+
+  // The inter-batch shuffling first groups subsets (i.e., micro-batches) with
+  // the same subset sum to minimize computation stalls. This forms shuffling
+  // ranges. Then, subsets within each shuffling range are shuffled.
   auto offsets = std::vector<std::size_t>();
   offsets.emplace_back(0);
 
-  for (std::size_t offset = 1; offset < num_micro_batches; ++offset) {
-    if (micro_batches[offset - 1].first != micro_batches[offset].first) {
+  for (std::size_t offset = 1; offset < num_subsets; ++offset) {
+    if (subsets[offset - 1].sum() != subsets[offset].sum()) {
       offsets.emplace_back(offset);
     }
   }
 
-  auto indices = std::vector<std::size_t>(num_micro_batches);
-  std::iota(indices.begin(), indices.end(), 0);
-
-  auto ranges = std::vector<std::pair<std::vector<std::size_t>::iterator,
-                                      std::vector<std::size_t>::iterator>>();
+  auto ranges = std::vector<std::pair<iterator, iterator>>();
   ranges.reserve(offsets.size());
 
-  for (std::size_t index = 0; index < offsets.size(); ++index) {
-    const auto begin = std::next(indices.begin(), offsets[index]);
-    if (index < offsets.size() - 1) {
-      const auto end = std::next(indices.begin(), offsets[index + 1]);
-      ranges.emplace_back(begin, end);
-    } else {
-      const auto end = indices.end();
-      ranges.emplace_back(begin, end);
-    }
+  for (std::size_t index = 0; index < offsets.size() - 1; ++index) {
+    const auto begin = std::next(subsets.begin(), offsets[index]);
+    const auto end = std::next(subsets.begin(), offsets[index + 1]);
+    ranges.emplace_back(begin, end);
   }
-
-  if (!use_flat_shuffle) {
-    auto generator = flatflow::aten::Generator(seed);
-    std::shuffle(ranges.begin(), ranges.end(), generator);
-  }
+  ranges.emplace_back(std::next(subsets.begin(), offsets.back()),
+                      subsets.end());
 
   std::for_each(std::execution::par, ranges.cbegin(), ranges.cend(),
                 [&](const auto &range) {
                   const auto begin = range.first;
                   const auto end = range.second;
-                  auto generator = flatflow::aten::Generator(seed);
+                  auto generator = std::mt19937();
+                  generator.seed(seed);
                   std::shuffle(begin, end, generator);
                 });
-
-  auto shuffled = std::vector<std::vector<std::pair<Size, Index>>>();
-  shuffled.reserve(num_micro_batches);
-
-  std::for_each(std::execution::seq, ranges.cbegin(), ranges.cend(),
-                [&](const auto &range) {
-                  std::for_each(std::execution::seq, range.first, range.second,
-                                [&](const auto index) {
-                                  shuffled.emplace_back(
-                                      std::move(micro_batches[index].second));
-                                });
-                });
-
-  return shuffled;
 }
 
 }  // namespace internal
