ad9ce2b345f37a334ddee30c0d254e3e66b66c94
9rum <sanha.maeng@kakao.com>
2025-02-07 20:06:29 +0900

    internal: use iterators

--- flatflow/scheduler/internal/shuffle.h
+++ flatflow/scheduler/internal/shuffle.h
@@ -16,66 +16,55 @@
 #define FLATFLOW_SCHEDULER_INTERNAL_SHUFFLE_H_
 
 #include <algorithm>
+#include <cstddef>
 #include <execution>
 #include <iterator>
 #include <random>
 #include <utility>
 #include <vector>
 
-#include "flatflow/data/internal/types.h"
-#include "flatflow/scheduler/internal/partition.h"
-
 namespace flatflow {
 namespace internal {
 
-// shuffle()
+// Shuffle()
 //
 // After partitioning, a `flatflow::Scheduler<>` shuffles between batches, which
 // we call inter-batch shuffling. This enables shuffling not only between data
 // samples with the same size but also between scheduled batches. We use the
 // same pseudo-random number generator and random seed as `flatflow::Dataset<>`
 // for deterministic shuffling.
-template <typename IndexType, typename R>
-  requires(Unsigned<IndexType> && Numerical<R>)
-void shuffle(std::vector<Subset<IndexType, R>> &subsets, IndexType seed) {
-  using iterator = typename std::vector<Subset<IndexType, R>>::iterator;
-
-  if (subsets.empty()) {
-    return;
-  }
-
-  const auto num_subsets = subsets.size();
+template <typename RandomIt>
+void Shuffle(RandomIt first, RandomIt last, std::mt19937::result_type seed) {
+  const auto m = std::distance(first, last);
 
   // The inter-batch shuffling first groups subsets (i.e., micro-batches) with
   // the same subset sum to minimize computation stalls. This forms shuffling
   // ranges. Then, subsets within each shuffling range are shuffled.
-  auto offsets = std::vector<std::size_t>();
+  auto offsets =
+      std::vector<typename std::iterator_traits<RandomIt>::difference_type>();
   offsets.emplace_back(0);
 
-  for (std::size_t offset = 1; offset < num_subsets; ++offset) {
-    if (subsets[offset - 1].sum() != subsets[offset].sum()) {
+  for (typename std::iterator_traits<RandomIt>::difference_type offset = 1;
+       offset < m; ++offset) {
+    if (*std::next(first, offset - 1) < *std::next(first, offset)) {
       offsets.emplace_back(offset);
     }
   }
 
-  auto ranges = std::vector<std::pair<iterator, iterator>>();
+  auto ranges = std::vector<std::pair<RandomIt, RandomIt>>();
   ranges.reserve(offsets.size());
 
   for (std::size_t index = 0; index < offsets.size() - 1; ++index) {
-    const auto begin = std::next(subsets.begin(), offsets[index]);
-    const auto end = std::next(subsets.begin(), offsets[index + 1]);
-    ranges.emplace_back(begin, end);
+    ranges.emplace_back(std::next(first, offsets[index]),
+                        std::next(first, offsets[index + 1]));
   }
-  ranges.emplace_back(std::next(subsets.begin(), offsets.back()),
-                      subsets.end());
+  ranges.emplace_back(std::next(first, offsets.back()), last);
 
   std::for_each(std::execution::par, ranges.cbegin(), ranges.cend(),
                 [&](const auto &range) {
-                  const auto begin = range.first;
-                  const auto end = range.second;
                   auto generator = std::mt19937();
                   generator.seed(seed);
-                  std::shuffle(begin, end, generator);
+                  std::shuffle(range.first, range.second, generator);
                 });
 }
 
