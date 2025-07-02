3a040164bd5a69f4dc756729ca6df774e360ea1a
Lee Hyun Joon <andylee70044@gmail.com>
2025-07-01 19:26:04 -0700

    language_modeling: add pretraining script megatron: update __init__ megatron: add gpt_dataset(need to update a base script for PR) langugae_modeling: modify megatron_gpt_model for pretraining

--- flatflow/nemo/collections/nlp/data/language_modeling/megatron/__init__.py
+++ flatflow/nemo/collections/nlp/data/language_modeling/megatron/__init__.py
@@ -1,12 +1,14 @@
 from flatflow.nemo.collections.nlp.data.language_modeling.megatron.blendable_dataset import (
     BlendableDataset,
 )
+from flatflow.nemo.collections.nlp.data.language_modeling.megatron.gpt_dataset import GPTDataset
 from flatflow.nemo.collections.nlp.data.language_modeling.megatron.gpt_sft_dataset import GPTSFTDataset
 from flatflow.nemo.collections.nlp.data.language_modeling.megatron.megatron_batch_samplers import (
     MegatronPretrainingBatchSampler,
 )
 
 __all__ = [
+    "GPTDataset",
     "GPTSFTDataset",
     "MegatronPretrainingBatchSampler",
     "BlendableDataset",
