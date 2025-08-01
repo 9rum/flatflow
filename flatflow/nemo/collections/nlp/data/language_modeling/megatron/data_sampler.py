3790a6ceb481b35e3153c3307ff3e7f0a6289c7e
Lee Hyun Joon <andylee70044@gmail.com>
2025-07-31 23:55:15 -0700

    megatron: apply flatflow for data_sampler

--- flatflow/nemo/collections/nlp/data/language_modeling/megatron/data_sampler.py
+++ flatflow/nemo/collections/nlp/data/language_modeling/megatron/data_sampler.py
@@ -19,196 +19,134 @@ from itertools import chain
 from typing import Optional
 
 import torch
+import nemo.collections.nlp.data.language_modeling.megatron.data_samplers
 
 from nemo.utils import logging
+import grpc
+import torch.distributed
+import torch.fx
+from megatron.core import parallel_state
+from flatflow import sys
+from flatflow.rpc import ControlPlaneClient, run
+from flatflow.torch.utils.data import Dataset
+
+__all__ = ["MegatronPretrainingSampler", "MegatronCorePretrainingSampler"]
 
 
-class BaseMegatronSampler:
+class MegatronPretrainingSampler(
+    nemo.collections.nlp.data.language_modeling.megatron.data_samplers.MegatronPretrainingSampler
+):
+    """Megatron-LM style pre-training sampler.
+
+    Args:
+        dataset (Dataset): Dataset used for sampling.
+        total_samples (int): Total number of samples in the dataset.
+        consumed_samples (int): Number of samples consumed by the model.
+        micro_batch_size (int): Micro batch size integer value.
+        global_batch_size (int): Global batch size integer value.
+          Calculated as data_parallel_size * per_replica_batch_size.
+        data_parallel_rank (int): Data parallel rank integer value.
+        data_parallel_size (int): Data parallel size integer value.
+        drop_last (bool): If ``True``, then the sampler will drop the
+            tail of the data to make it evenly divisible across the number of
+            replicas. If ``False``, the sampler will add extra indices to make
+            the data evenly divisible across the replicas. (default: ``False``)
+        graph (torch.fx.Graph): The exported computational graph.
+    """
+
     def __init__(
         self,
+        dataset: Dataset,
         total_samples: int,
         consumed_samples: int,
         micro_batch_size: int,
+        global_batch_size: int,
         data_parallel_rank: int,
         data_parallel_size: int,
-        drop_last: bool = True,
-        global_batch_size: Optional[int] = None,
-        rampup_batch_size: Optional[list] = None,
-        pad_samples_to_global_batch_size: Optional[bool] = False,
-    ) -> None:
-        # Sanity checks.
-        if total_samples <= 0:
-            raise RuntimeError("no sample to consume: {}".format(total_samples))
-        if micro_batch_size <= 0:
-            raise RuntimeError(f"micro_batch_size size must be greater than 0, but {micro_batch_size}")
-        if data_parallel_size <= 0:
-            raise RuntimeError(f"data parallel size must be greater than 0, but {data_parallel_size}")
-        if data_parallel_rank >= data_parallel_size:
-            raise RuntimeError(
-                "data_parallel_rank should be smaller than data size, but {} >= {}".format(
-                    data_parallel_rank, data_parallel_size
-                )
-            )
-        if global_batch_size is not None and rampup_batch_size is None:
-            if global_batch_size % (micro_batch_size * data_parallel_size) != 0:
-                raise RuntimeError(
-                    f"`global_batch_size` ({global_batch_size}) is not divisible by "
-                    f"`micro_batch_size ({micro_batch_size}) x data_parallel_size "
-                    f"({data_parallel_size})`"
-                )
-        if pad_samples_to_global_batch_size and global_batch_size is None:
-            raise RuntimeError(
-                f"`pad_samples_to_global_batch_size` can be `True` only when "
-                f"`global_batch_size` is set to an integer value"
-            )
-
-        # Keep a copy of input params for later use.
-        self.total_samples = total_samples
-        self.consumed_samples = consumed_samples
-        self.micro_batch_size = micro_batch_size
-        self.data_parallel_rank = data_parallel_rank
-        self.data_parallel_size = data_parallel_size
-        self.micro_batch_times_data_parallel_size = self.micro_batch_size * data_parallel_size
-        self.drop_last = drop_last
-        self.global_batch_size = global_batch_size
-        self.pad_samples_to_global_batch_size = pad_samples_to_global_batch_size
-
-        logging.info(
-            f'Instantiating MegatronPretrainingSampler with total_samples: {total_samples} and consumed_samples: {consumed_samples}'
-        )
-
-    def __len__(self):
-        num_available_samples: int = self.total_samples - self.consumed_samples
-        if self.global_batch_size is not None:
-            if self.drop_last:
-                num_global_batches = num_available_samples // self.global_batch_size
-            else:
-                num_global_batches = (num_available_samples + self.global_batch_size - 1) // self.global_batch_size
-            # return len of dataloader in terms of micro batches to avoid discrepancy between len of dataloader and
-            # num of batches fetched (as training step fetches in terms of micro batches)
-            return num_global_batches * (self.global_batch_size // self.micro_batch_times_data_parallel_size)
-        else:
-            return (num_available_samples - 1) // self.micro_batch_times_data_parallel_size + 1
-
-    @abc.abstractmethod
-    def __iter__(self): ...
-
-
-class MegatronPretrainingSampler(BaseMegatronSampler):
-    def get_start_end_idx(self):
-        start_idx = self.data_parallel_rank * self.micro_batch_size
-        end_idx = start_idx + self.micro_batch_size
-        return start_idx, end_idx
-
-    def _get_padding_indices(self, pad_samples_num):
-        return range(-1, -pad_samples_num - 1, -1)
-
-    def __iter__(self):
-        batch = []
-        # Last batch will be dropped if drop_last is not set False
-        indices = range(self.consumed_samples, self.total_samples)
-        if (not self.drop_last) and self.pad_samples_to_global_batch_size:
-            pad_samples_num = -len(indices) % self.global_batch_size
-            pad_indices = self._get_padding_indices(pad_samples_num)
-            indices = chain(indices, pad_indices)
-
-        for idx in indices:
-            batch.append(idx)
-            if len(batch) == self.micro_batch_times_data_parallel_size:
-                start_idx, end_idx = self.get_start_end_idx()
-                yield batch[start_idx:end_idx]
-                batch = []
-
-        # Check the last partial batch and see drop_last is set
-        if len(batch) > 0 and not self.drop_last:
-            assert (
-                not self.pad_samples_to_global_batch_size
-            ), 'with pad_samples_to_global_batch_size all batches should be complete'
-            start_idx, end_idx = self.get_start_end_idx()
-            yield batch[start_idx:end_idx]
-
-
-class MegatronCorePretrainingSampler(MegatronPretrainingSampler):
-    def _get_padding_indices(self, pad_samples_num):
-        return [None] * pad_samples_num
-
-
-class MegatronPretrainingRandomSampler(BaseMegatronSampler):
-    def __init__(
-        self,
-        total_samples: int,
-        consumed_samples: int,
-        micro_batch_size: int,
-        data_parallel_rank: int,
-        data_parallel_size: int,
-        drop_last: bool = True,
-        global_batch_size: Optional[int] = None,
-        pad_samples_to_global_batch_size: Optional[bool] = False,
-        seed: int = 0,
+        drop_last: bool,
+        graph: torch.fx.Graph,
+        *args, **kwargs,
     ) -> None:
         super().__init__(
             total_samples=total_samples,
             consumed_samples=consumed_samples,
             micro_batch_size=micro_batch_size,
+            global_batch_size=global_batch_size,
             data_parallel_rank=data_parallel_rank,
             data_parallel_size=data_parallel_size,
             drop_last=drop_last,
-            global_batch_size=global_batch_size,
-            pad_samples_to_global_batch_size=pad_samples_to_global_batch_size,
+            *args, **kwargs,
         )
-        assert (
-            not pad_samples_to_global_batch_size
-        ), "`MegatronPretrainingRandomSampler` does not support sample padding"
-        if (not drop_last) and self.micro_batch_times_data_parallel_size > 1:
-            raise RuntimeError(
-                "`MegatronPretrainingRandomSampler` does not support drop_last=False when micro_batch_size * data_parallel_size > 1. \
-                  please reduce your MBS and data parallelism to 1 if you want to use drop_last=False, or switch to drop_last=True to avoid this error"
-            )
-        self.last_batch_size = self.total_samples % self.micro_batch_times_data_parallel_size
-        self.seed = seed
+        self.dataset = dataset
+        self.epoch = 0
 
-    def __len__(self):
-        active_total_samples = self.total_samples - (self.last_batch_size if self.drop_last else 0)
-        num_available_samples = active_total_samples - self.consumed_samples % active_total_samples
-        if self.global_batch_size is not None:
-            if self.drop_last:
-                num_global_batches = num_available_samples // self.global_batch_size
-            else:
-                num_global_batches = (num_available_samples + self.global_batch_size - 1) // self.global_batch_size
-            # return len of dataloader in terms of micro batches to avoid discrepancy between len of dataloader and
-            # num of batches fetched (as training step fetches in terms of micro batches)
-            return num_global_batches * (self.global_batch_size // self.micro_batch_times_data_parallel_size)
-        else:
-            if self.drop_last:
-                return num_available_samples // self.micro_batch_times_data_parallel_size
-            else:
-                return (num_available_samples - 1) // self.micro_batch_times_data_parallel_size
+        pipeline_parallel_rank = parallel_state.get_pipeline_model_parallel_rank()
+        tensor_parallel_rank = parallel_state.get_tensor_model_parallel_rank()
+        if pipeline_parallel_rank == 0 and tensor_parallel_rank == 0:
+            # Launch a control plane for this worker. Network communication is avoided
+            # so port conflicts are handled internally by the extension.
+            port = run()
+
+            # The control plane runs locally on every data parallel worker and
+            # communicates through the IPv6 loopback interface only.
+            channel = grpc.insecure_channel(f"[::1]:{port}")
+            self.client = ControlPlaneClient(channel)
+            sizes = [
+                sys.getsizeof(dataset, index)
+                if index < len(dataset)
+                else sys.getsizeof(dataset, len(dataset) - 1)
+                for index in range(self.total_samples)
+            ]
+            self.client.Init(
+                data_parallel_rank,
+                data_parallel_size,
+                global_batch_size,
+                micro_batch_size,
+                graph,
+                sizes,
+            )
+
+    def set_epoch(self, epoch: int) -> None:
+        """Sets the epoch for this sampler. This ensures all replicas use a different random ordering for each epoch.
+        Otherwise, the next iteration of this sampler will yield the same ordering.
+
+        Args:
+            epoch (int): Epoch number.
+        """
+        self.epoch = epoch
 
     def __iter__(self):
-        active_total_samples = self.total_samples - self.last_batch_size
-        self.epoch = self.consumed_samples // active_total_samples
-        current_epoch_samples = self.consumed_samples % active_total_samples
-        assert current_epoch_samples % self.micro_batch_times_data_parallel_size == 0
+        model_parallel_group = parallel_state.get_model_parallel_group()
+        model_parallel_src_rank = torch.distributed.get_process_group_ranks(model_parallel_group)[0]
+        rank = torch.distributed.get_rank()
+        is_model_parallel_src = rank == model_parallel_src_rank
 
-        # data sharding and random sampling
-        bucket_size = (self.total_samples // self.micro_batch_times_data_parallel_size) * self.micro_batch_size
-        bucket_offset = current_epoch_samples // self.data_parallel_size
-        start_idx = self.data_parallel_rank * bucket_size
-
-        g = torch.Generator()
-        g.manual_seed(self.seed + self.epoch)
-        random_idx = torch.randperm(bucket_size, generator=g).tolist()
-        idx_range = [start_idx + x for x in random_idx[bucket_offset:]]
+        # Receive the reordered computation schedule from the control plane.
+        if is_model_parallel_src:
+            indices = list(range(self.total_samples))
+            self._indices = self.client.Scatter(self.epoch, indices)
+            self.epoch += 1
+        torch.distributed.broadcast_object_list(self._indices, model_parallel_src_rank, model_parallel_group)
 
         batch = []
-        # Last batch if not complete will be dropped.
-        for idx in idx_range:
+        for idx in self._indices:  # type: ignore[attr-defined]
             batch.append(idx)
-            if len(batch) == self.micro_batch_size:
-                self.consumed_samples += self.micro_batch_times_data_parallel_size
-                yield batch
+            if len(batch) == self.micro_batch_times_data_parallel_size:
+                start, end = self.get_start_end_idx()
+                yield batch[start:end]   # yield per micro_batch_size
                 batch = []
 
-        # Check the last partial batch and see drop_last is set
-        if len(batch) > 0 and not self.drop_last:
-            yield batch
+        if len(batch) and not self.drop_last:
+            start, end = self.get_start_end_idx()
+            yield batch[start:end]
+
+    def __del__(self) -> None:
+        if hasattr(self, "client"):
+            self.client.Finalize()
+
+
+class MegatronCorePretrainingSampler(MegatronPretrainingSampler):
+    """_get_padding_indices is different"""
+
+    def _get_padding_indices(self, pad_samples_num: int):
+        return [None] * pad_samples_num
