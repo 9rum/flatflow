15f3c761095bc75c7ca66c94c8e923e82022846c
Lee Hyun Joon <andylee70044@gmail.com>
2025-08-18 12:07:48 -0700

    megatron: fix file name

--- flatflow/nemo/collections/nlp/data/language_modeling/megatron/__init__.py
+++ flatflow/nemo/collections/nlp/data/language_modeling/megatron/__init__.py
@@ -1,7 +1,7 @@
 from flatflow.nemo.collections.nlp.data.language_modeling.megatron.blendable_dataset import BlendableDataset
 from flatflow.nemo.collections.nlp.data.language_modeling.megatron.gpt_sft_dataset import GPTSFTDataset
 from flatflow.nemo.collections.nlp.data.language_modeling.megatron.megatron_batch_samplers import MegatronPretrainingBatchSampler
-from flatflow.nemo.collections.nlp.data.language_modeling.megatron.data_sampler import MegatronPretrainingSampler, MegatronCorePretrainingSampler
+from flatflow.nemo.collections.nlp.data.language_modeling.megatron.data_samplers import MegatronPretrainingSampler, MegatronCorePretrainingSampler
 
 __all__ = [
     "BlendableDataset",
