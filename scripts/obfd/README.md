9cc62eecf10e34ea8a714168d9582b31243b9a78
9rum <sanha.maeng@kakao.com>
2025-08-25 20:08:00 +0900

    obfd: implement per-doc chunking

---
+++ scripts/obfd/README.md
@@ -0,0 +1,45 @@
+This is a naive implementation of [Fewer Truncations Improve Language Modeling](https://proceedings.mlr.press/v235/ding24f.html) [ICML '24].
+
+## Prerequisites
+
+You have to install several dependencies before running the scripts:
+
+```bash
+$ pip install -r scripts/obfd/requirements.txt
+```
+
+## Per-doc chunking
+
+The proposed approach requires per-doc chunking before document packing.
+Run the following command to perform per-doc chunking:
+
+```bash
+$ python3 scripts/obfd/chunk.py \
+    --input=PATH_TO_JSON_FILE \
+    --json-key=JSON_KEY \
+    --tokenizer-library=LIBRARY \
+    --tokenizer-type=MODEL_NAME \
+    --output-prefix=PATH_TO_OUTPUT_DIR \
+    --need-pad-id \
+    --max-seq-len=MAX_SEQ_LEN
+```
+
+If you preprocess [RefinedWeb](https://huggingface.co/datasets/tiiuae/falcon-refinedweb) for Hugging Face Llma 3.2 1B with maximum sequence length of 8192, then run the following command:
+
+```bash
+$ python3 scripts/obfd/chunk.py \
+    --input=PATH_TO_JSON_FILE \
+    --json-key=content \
+    --tokenizer-library=huggingface \
+    --tokenizer-type=meta-llama/Llama-3.2-1B \
+    --output-prefix=PATH_TO_OUTPUT_DIR \
+    --need-pad-id \
+    --max-seq-len=8192
+```
+
+> [!CAUTION]
+> This script assumes that the dataset files are merged into a single .jsonl file.
+
+This may produce a .jsonl file containing the chunked documents, and its metadata .npy file.
+
+## Best-fit packing
---
+++ scripts/obfd/chunk.py
@@ -0,0 +1,89 @@
+"""
+Unofficial implementation of Fewer Truncations Improve Language Modeling [ICML '24].
+"""
+
+import argparse
+import json
+import os
+import time
+
+import numpy as np
+from nemo.collections.nlp.modules.common.tokenizer_utils import get_nmt_tokenizer
+from tqdm import tqdm
+
+
+def get_tokenizer(args):
+    tokenizer = get_nmt_tokenizer(
+        library=args.tokenizer_library,
+        model_name=args.tokenizer_type,
+    )
+    if args.need_pad_id:
+        if (
+            not hasattr(tokenizer, "pad_id")
+            or tokenizer.pad_id is None  # type: ignore
+            or tokenizer.pad_id < 0  # type: ignore
+        ):
+            tokenizer.add_special_tokens({"pad_token": "<pad>"})  # type: ignore
+    return tokenizer
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", type=str, required=True)
+    parser.add_argument("--json-key", type=str, default="content")
+    parser.add_argument(
+        "--tokenizer-library",
+        type=str,
+        default="huggingface",
+        choices=["sentencepiece", "megatron", "huggingface", "tabular"],
+    )
+    parser.add_argument("--tokenizer-type", type=str, required=True)
+    parser.add_argument("--need-pad-id", action="store_true")
+    parser.add_argument("--max-seq-len", type=int, default=8192)
+    parser.add_argument("--output-prefix", type=str, required=True)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    now = time.monotonic()
+
+    args = get_args()
+    assert os.path.exists(args.input), f"File does not exist: {args.input}"
+
+    tokenizer = get_tokenizer(args)
+
+    print(f"Processing file {args.input}")
+    filename = os.path.splitext(os.path.basename(args.input))[0]
+    fin = open(args.input, "r", encoding="utf-8")
+    fout = open(f"{args.output_prefix}{filename}_chunk.jsonl", "w", encoding="utf-8")
+    token_counts = []
+
+    for line in tqdm(fin):
+        data = json.loads(line)
+        text = data[args.json_key]
+        ids = tokenizer.text_to_ids(text)
+        num_tokens = len(ids)
+
+        if args.max_seq_len < num_tokens:
+            # Perform per-doc chunking based on Figure 1 in `Fewer Truncations Improve
+            # Language Modeling`. See https://openreview.net/pdf?id=kRxCDDFNpp.
+            for idx in range(0, num_tokens, args.max_seq_len):
+                chunk_ids = ids[idx : idx + args.max_seq_len]
+                chunk_text = tokenizer.ids_to_text(chunk_ids)
+                token_counts.append(len(chunk_ids))
+                fout.write(
+                    json.dumps({args.json_key: chunk_text}, ensure_ascii=False) + "\n"
+                )
+        else:
+            token_counts.append(num_tokens)
+            fout.write(json.dumps(data, ensure_ascii=False) + "\n")
+
+    fin.close()
+    fout.close()
+    np.save(f"{args.output_prefix}{filename}_chunk_meta.npy", np.array(num_tokens))
+    print(f"Took {time.monotonic() - now}s for per-doc chunking")
+
+
+if __name__ == "__main__":
+    main()
