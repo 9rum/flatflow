d8fa9bccffbde4750adefabd06d460654f1f3746
Lee Hyun Joon <99354933+amo33@users.noreply.github.com>
2025-09-01 15:51:13 +0900

    megatron: apply scripts for pretraining task (#39)

--- flatflow/nemo/collections/nlp/models/language_modeling/__init__.py
+++ flatflow/nemo/collections/nlp/models/language_modeling/__init__.py
@@ -1,3 +1,4 @@
+from flatflow.nemo.collections.nlp.models.language_modeling.megatron_gpt_model import MegatronGPTModel
 from flatflow.nemo.collections.nlp.models.language_modeling.megatron_gpt_sft_model import MegatronGPTSFTModel
 
-__all__ = ["MegatronGPTSFTModel"]
+__all__ = ["MegatronGPTModel", "MegatronGPTSFTModel"]
