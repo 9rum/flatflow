e91ba21527a6a1391ec806d0244cec49e0821902
9rum <sanha.maeng@kakao.com>
2025-11-10 19:38:52 +0900

    energon: remove task encoder

--- flatflow/megatron/energon/__init__.py
+++
@@ -1,3 +0,0 @@
-from flatflow.megatron.energon.task_encoder import MegatronTaskEncoder
-
-__all__ = ["MegatronTaskEncoder"]
--- flatflow/megatron/energon/task_encoder/__init__.py
+++
@@ -1,3 +0,0 @@
-from flatflow.megatron.energon.task_encoder.megatron import MegatronTaskEncoder
-
-__all__ = ["MegatronTaskEncoder"]
