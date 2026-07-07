6331cf71516ec434090f7b3e6e9de2071cd67a79
9rum <sanha.maeng@kakao.com>
2026-07-07 17:50:31 +0900

    sched: implement stable joint optimization scheduler

--- src/sched.rs
+++ src/sched.rs
@@ -52,6 +52,89 @@ impl From<&str> for Policy {
 
 #[pyfunction]
 #[inline]
+pub fn sched(
+    indices: Vec<usize>,
+    sizes: Vec<i64>,
+    buf: &[u8],
+    tensor_parallel_world_size: i64,
+    context_parallel_world_size: i64,
+    data_parallel_world_size: usize,
+    data_parallel_rank: usize,
+    global_batch_size: usize,
+    micro_batch_size: usize,
+    policy: &str,
+) -> PyResult<Vec<usize>> {
+    assert_ne!(data_parallel_world_size, 0);
+    assert_eq!(indices.len() % data_parallel_world_size, 0);
+    assert_ne!(micro_batch_size, 0);
+
+    let now = Instant::now();
+    let num_samples = indices.len() / data_parallel_world_size;
+    defer!(info!(
+        "Reordering {} micro-batches took {:?}",
+        ((num_samples - 1) / micro_batch_size + 1) * data_parallel_world_size,
+        now.elapsed()
+    ));
+
+    let mut indices = indices;
+
+    match policy.into() {
+        Policy::Joint => sched_joint(
+            indices.as_mut_slice(),
+            sizes.as_slice(),
+            buf,
+            tensor_parallel_world_size,
+            context_parallel_world_size,
+            data_parallel_world_size,
+            data_parallel_rank,
+            global_batch_size,
+            micro_batch_size,
+        ),
+        Policy::Fast => todo!(),
+        Policy::Mem => todo!(),
+    }
+    .map_err(|err| PyValueError::new_err(err.to_string()))
+}
+
+fn sched_joint(
+    indices: &mut [usize],
+    sizes: &[i64],
+    buf: &[u8],
+    tensor_parallel_world_size: i64,
+    context_parallel_world_size: i64,
+    data_parallel_world_size: usize,
+    data_parallel_rank: usize,
+    global_batch_size: usize,
+    micro_batch_size: usize,
+) -> Result<Vec<usize>, InvalidFlatbuffer> {
+    assert_eq!(global_batch_size % data_parallel_world_size, 0);
+    let per_replica_batch_size = global_batch_size / data_parallel_world_size;
+    let gradient_accumulation_steps = per_replica_batch_size / micro_batch_size;
+
+    let proj =
+        transform(root_as_graph(buf)?, tensor_parallel_world_size, context_parallel_world_size);
+
+    // Partition the given indices in a memory-balanced manner so that the number of tokens in each
+    // batch is as uniform as possible.
+    indices.sort_unstable_by_key(|&index| sizes[index]);
+    let batches: Vec<Vec<_>> =
+        partition(indices.iter().copied(), data_parallel_world_size, |&index| sizes[index], None);
+
+    let mut batch = batches.into_iter().nth(data_parallel_rank).unwrap();
+    batch.sort_unstable_by_key(|&index| proj(sizes[index]));
+
+    // Partition the batch into micro-batches so that any earlier micro-batch takes less execution
+    // time than subsequent ones to reduce pipeline bubbles.
+    let last_micro_batch_size = batch.len() % micro_batch_size;
+    let last_micro_batch = batch.split_off(batch.len() - last_micro_batch_size);
+    let micro_batches: Vec<Vec<_>> =
+        partition(batch, gradient_accumulation_steps, |&index| proj(sizes[index]), None);
+
+    Ok(micro_batches.into_iter().chain(once(last_micro_batch)).flatten().collect())
+}
+
+#[pyfunction]
+#[inline]
 pub fn sched_unstable(
     indices: Vec<usize>,
     sizes: Vec<i64>,
