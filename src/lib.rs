ff5062c4b4603804276895bf7651264ed7174d4c
9rum <sanha.maeng@kakao.com>
2026-07-08 11:45:12 +0900

    src: use inline module syntax

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,31 @@
 // SPDX-License-Identifier: Apache-2.0
 
-use pyo3::types::{PyModule, PyModuleMethods};
-use pyo3::{Bound, PyResult, pymodule, wrap_pyfunction};
+use pyo3::pymodule;
 
 pub mod ops;
 pub mod sched;
 
 #[pymodule]
-fn ffi<'py>(m: &Bound<'py, PyModule>) -> PyResult<()> {
-    pyo3_log::init();
+mod ffi {
+    use pyo3::types::PyModule;
+    use pyo3::{Bound, PyResult};
 
     // This may bind `flatflow::sched::sched` and `flatflow::sched::sched_unstable` to
     // `flatflow.ffi.sched` and `flatflow.ffi.sched_unstable` in the Python frontend, respectively.
-    m.add_function(wrap_pyfunction!(sched::sched, m)?)?;
-    m.add_function(wrap_pyfunction!(sched::sched_unstable, m)?)?;
+    #[pymodule_export]
+    use super::sched::sched;
+    #[pymodule_export]
+    use super::sched::sched_unstable;
 
-    Ok(())
+    #[pymodule_export]
+    #[allow(non_upper_case_globals)]
+    const __version__: &str = env!("CARGO_PKG_VERSION");
+
+    #[pymodule_init]
+    #[allow(unused_variables)]
+    fn init(m: &Bound<'_, PyModule>) -> PyResult<()> {
+        pyo3_log::init();
+
+        Ok(())
+    }
 }
